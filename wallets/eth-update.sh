f772453939d614ee8141c5c611f8503558a21e80
Neal Conner <neal@lamassu.is>
2019-09-20 10:16:12 -0400

    Create eth-update.sh

---
+++ wallets/eth-update.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+export LOG_FILE=/tmp/eth-update.$(date +"%Y%m%d").log
+
+echo
+echo "Updating Ethereum. This may take a minute."
+supervisorctl stop ethereum >> ${LOG_FILE} 2>&1
+echo
+echo "Downloading..."
+curl -#o /tmp/ethereum.tar.gz https://gethstore.blob.core.windows.net/builds/geth-linux-amd64-1.9.5-a1c09b93.tar.gz >> ${LOG_FILE} 2>&1
+tar -xzf /tmp/ethereum.tar.gz -C /tmp/ >> ${LOG_FILE} 2>&1
+echo
+echo "Updating..."
+mv /usr/local/bin/geth /usr/local/bin/geth-old >> ${LOG_FILE} 2>&1
+cp /tmp/geth-linux-amd64-1.9.5-a1c09b93/geth /usr/local/bin/geth >> ${LOG_FILE} 2>&1
+rm -r /tmp/geth-linux-amd64-1.9.5-a1c09b93/ >> ${LOG_FILE} 2>&1
+rm /tmp/ethereum.tar.gz >> ${LOG_FILE} 2>&1
+supervisorctl start ethereum >> ${LOG_FILE} 2>&1
+echo
+echo "Ethereum is updated."
