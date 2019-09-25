cc2e185cc5956f86caec37ec0f0af5f4db3f45b4
Neal Conner <neal@lamassu.is>
2019-09-25 16:56:59 -0400

    Update ZEC

--- wallets/zec-update.sh
+++ wallets/zec-update.sh
@@ -7,8 +7,8 @@ echo
 echo "Updating your Zcash wallet. This may take a few minutes."
 supervisorctl stop zcash >> ${LOG_FILE} 2>&1
 echo
-echo "Downloading Zcash v2.0.7-2..."
-curl -#Lo /tmp/zcash.tar.gz https://z.cash/downloads/zcash-2.0.7-2-linux64-debian-jessie.tar.gz >> ${LOG_FILE} 2>&1
+echo "Downloading Zcash v2.0.7-3..."
+curl -#Lo /tmp/zcash.tar.gz https://z.cash/downloads/zcash-2.0.7-3-linux64-debian-jessie.tar.gz >> ${LOG_FILE} 2>&1
 tar -xzf /tmp/zcash.tar.gz -C /tmp/ >> ${LOG_FILE} 2>&1
 echo
 echo "Updating..."
@@ -16,10 +16,9 @@ mv /usr/local/bin/zcashd /usr/local/bin/zcashd-old >> ${LOG_FILE} 2>&1
 mv /usr/local/bin/zcash-cli /usr/local/bin/zcash-cli-old >> ${LOG_FILE} 2>&1
 mv /usr/local/bin/zcash-tx /usr/local/bin/zcash-tx-old >> ${LOG_FILE} 2>&1
 mv /usr/local/bin/zcash-fetch-params /usr/local/bin/zcash-fetch-params-old >> ${LOG_FILE} 2>&1
-cp /tmp/zcash-2.0.7-2/bin/* /usr/local/bin/ >> ${LOG_FILE} 2>&1
-rm -r /tmp/zcash-2.0.7-2 >> ${LOG_FILE} 2>&1
+cp /tmp/zcash-2.0.7-3/bin/* /usr/local/bin/ >> ${LOG_FILE} 2>&1
+rm -r /tmp/zcash-2.0.7-3 >> ${LOG_FILE} 2>&1
 rm /tmp/zcash.tar.gz >> ${LOG_FILE} 2>&1
-curl -#o /etc/supervisor/conf.d/zcash.conf https://raw.githubusercontent.com/LamassuSupport/lamascripts/master/updates/nodes/zcash.conf >> ${LOG_FILE} 2>&1
 echo
 echo "Downloading Sapling parameters, this will take a minute or two..."
 zcash-fetch-params >> ${LOG_FILE} 2>&1
