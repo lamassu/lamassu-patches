d9c1412fa2b88fbea3634614eb615d26805f43d5
naconner <7396812+naconner@users.noreply.github.com>
2019-12-31 16:30:41 -0500

    litecoind 0.17.1

--- wallets/ltc-update.sh
+++ wallets/ltc-update.sh
@@ -8,7 +8,8 @@ echo "Updating Litecoin Core. This may take a minute."
 supervisorctl stop litecoin >> ${LOG_FILE} 2>&1
 echo
 echo "Downloading..."
-curl -#o /tmp/litecoin.tar.gz https://download.litecoin.org/litecoin-0.16.3/linux/litecoin-0.16.3-x86_64-linux-gnu.tar.gz >> ${LOG_FILE} 2>&1
+curl -#o $(npm root -g)/lamassu-server/lib/plugins/wallet/litecoind/litecoind.js https://raw.githubusercontent.com/lamassu/lamassu-server/defiant-dingirma/lib/plugins/wallet/litecoind/litecoind.js
+curl -#o /tmp/litecoin.tar.gz https://download.litecoin.org/litecoin-0.17.1/linux/litecoin-0.17.1-x86_64-linux-gnu.tar.gz >> ${LOG_FILE} 2>&1
 tar -xzf /tmp/litecoin.tar.gz -C /tmp/ >> ${LOG_FILE} 2>&1
 echo
 echo "Updating..."
@@ -16,10 +17,11 @@ mv /usr/local/bin/litecoind /usr/local/bin/litecoind-old >> ${LOG_FILE} 2>&1
 mv /usr/local/bin/litecoin-cli /usr/local/bin/litecoin-cli-old >> ${LOG_FILE} 2>&1
 mv /usr/local/bin/litecoin-tx /usr/local/bin/litecoin-tx-old >> ${LOG_FILE} 2>&1
 mv /usr/local/bin/litecoin-qt /usr/local/bin/litecoin-qt-old >> ${LOG_FILE} 2>&1
-cp /tmp/litecoin-0.16.3/bin/* /usr/local/bin/ >> ${LOG_FILE} 2>&1
-rm -r /tmp/litecoin-0.16.3 >> ${LOG_FILE} 2>&1
+cp /tmp/litecoin-0.17.1/bin/* /usr/local/bin/ >> ${LOG_FILE} 2>&1
+rm -r /tmp/litecoin-0.17.1 >> ${LOG_FILE} 2>&1
 rm /tmp/litecoin.tar.gz >> ${LOG_FILE} 2>&1
 supervisorctl start litecoin >> ${LOG_FILE} 2>&1
+supervisorctl restart lamassu-server lamassu-admin-server >> ${LOG_FILE} 2>&1
 echo
 echo "Litecoin Core is updated."
 echo
