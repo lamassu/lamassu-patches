7ec6f482747979286a6b7d7900ab3df4d0631238
naconner <7396812+naconner@users.noreply.github.com>
2019-12-31 16:37:07 -0500

    litecoind 0.17.1

--- wallets/ltc-update.sh
+++ wallets/ltc-update.sh
@@ -8,7 +8,7 @@ echo "Updating Litecoin Core. This may take a minute."
 supervisorctl stop litecoin >> ${LOG_FILE} 2>&1
 echo
 echo "Downloading..."
-curl -#o $(npm root -g)/lamassu-server/lib/plugins/wallet/litecoind/litecoind.js https://raw.githubusercontent.com/lamassu/lamassu-server/defiant-dingirma/lib/plugins/wallet/litecoind/litecoind.js
+curl -#o $(npm root -g)/lamassu-server/lib/plugins/wallet/litecoind/litecoind.js https://raw.githubusercontent.com/lamassu/lamassu-server/defiant-dingirma/lib/plugins/wallet/litecoind/litecoind.js >> ${LOG_FILE} 2>&1
 curl -#o /tmp/litecoin.tar.gz https://download.litecoin.org/litecoin-0.17.1/linux/litecoin-0.17.1-x86_64-linux-gnu.tar.gz >> ${LOG_FILE} 2>&1
 tar -xzf /tmp/litecoin.tar.gz -C /tmp/ >> ${LOG_FILE} 2>&1
 echo
