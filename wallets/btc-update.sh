7455fdf650f9e9cc5c44e2e37109a6893f696ef8
naconner <7396812+naconner@users.noreply.github.com>
2020-01-15 15:30:45 -0500

    fix: use bitcoind 0.16.3 for now
    bitcoind 0.17 and above breaks how we're considering unconfirmed deposits and change.

--- wallets/btc-update.sh
+++ wallets/btc-update.sh
@@ -9,7 +9,7 @@ supervisorctl stop bitcoin >> ${LOG_FILE} 2>&1
 echo
 echo "Downloading..."
 curl -#o $(npm root -g)/lamassu-server/lib/plugins/wallet/bitcoind/bitcoind.js https://raw.githubusercontent.com/lamassu/lamassu-server/defiant-dingirma/lib/plugins/wallet/bitcoind/bitcoind.js >> ${LOG_FILE} 2>&1
-curl -#o /tmp/bitcoin.tar.gz https://bitcoin.org/bin/bitcoin-core-0.19.0.1/bitcoin-0.19.0.1-x86_64-linux-gnu.tar.gz >> ${LOG_FILE} 2>&1
+curl -#o /tmp/bitcoin.tar.gz https://bitcoin.org/bin/bitcoin-core-0.16.3/bitcoin-0.16.3-x86_64-linux-gnu.tar.gz >> ${LOG_FILE} 2>&1
 tar -xzf /tmp/bitcoin.tar.gz -C /tmp/ >> ${LOG_FILE} 2>&1
 echo
 echo "Updating..."
@@ -17,8 +17,8 @@ mv /usr/local/bin/bitcoind /usr/local/bin/bitcoind-old >> ${LOG_FILE} 2>&1
 mv /usr/local/bin/bitcoin-cli /usr/local/bin/bitcoin-cli-old >> ${LOG_FILE} 2>&1
 mv /usr/local/bin/bitcoin-tx /usr/local/bin/bitcoind-tx-old >> ${LOG_FILE} 2>&1
 mv /usr/local/bin/bitcoin-qt /usr/local/bin/bitcoind-qt-old >> ${LOG_FILE} 2>&1
-cp /tmp/bitcoin-0.19.0.1/bin/* /usr/local/bin/ >> ${LOG_FILE} 2>&1
-rm -r /tmp/bitcoin-0.19.0.1 >> ${LOG_FILE} 2>&1
+cp /tmp/bitcoin-0.16.3/bin/* /usr/local/bin/ >> ${LOG_FILE} 2>&1
+rm -r /tmp/bitcoin-0.16.3 >> ${LOG_FILE} 2>&1
 rm /tmp/bitcoin.tar.gz >> ${LOG_FILE} 2>&1
 supervisorctl start bitcoin >> ${LOG_FILE} 2>&1
 supervisorctl restart lamassu-server lamassu-admin-server >> ${LOG_FILE} 2>&1
