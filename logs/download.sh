e0ec20731158fe578ede934e9946fcad5567923d
naconner <7396812+naconner@users.noreply.github.com>
2020-01-28 10:47:25 -0500

    Update download.sh

--- logs/download.sh
+++ logs/download.sh
@@ -6,32 +6,33 @@ normal=$(tput sgr0)
 apt-get -qq update
 if [ $(dpkg-query -W -f='${Status}' mutt 2>/dev/null | grep -c "ok installed") -eq 0 ];
 then
+  echo
   echo "Mutt not installed. Installing..."
-  DEBIAN_FRONTEND=noninteractive apt-get install -y --force-yes mutt >/dev/null;
+  DEBIAN_FRONTEND=noninteractive apt-get install -y --force-yes mutt &>/dev/null;
 fi
 mkdir -p /etc/lamassu/keys/
-curl -#o /etc/lamassu/keys/lamassu-log-server.key https://raw.githubusercontent.com/lamassu/lamassu-patches/master/logs/lamassu-log-server.key 2>/dev/null
+curl -#o /etc/lamassu/keys/lamassu-log-server.key https://raw.githubusercontent.com/lamassu/lamassu-patches/master/logs/lamassu-log-server.key &>/dev/null
 chmod ga-rwx /etc/lamassu/keys/lamassu-log-server.key
-curl -#o /usr/local/bin/cash-in-log https://raw.githubusercontent.com/lamassu/lamassu-patches/master/logs/cash-in-log 2>/dev/null
-curl -#o /usr/local/bin/cash-out-log https://raw.githubusercontent.com/lamassu/lamassu-patches/master/logs/cash-out-log 2>/dev/null
-curl -#o /usr/local/bin/bills-log https://raw.githubusercontent.com/lamassu/lamassu-patches/master/logs/bills-log 2>/dev/null
-curl -#o /usr/local/bin/cash-in-unencrypted https://raw.githubusercontent.com/lamassu/lamassu-patches/master/logs/cash-in-unencrypted 2>/dev/null
-curl -#o /usr/local/bin/cash-out-unencrypted https://raw.githubusercontent.com/lamassu/lamassu-patches/master/logs/cash-out-unencrypted 2>/dev/null
-curl -#o /usr/local/bin/bills-log-unencrypted https://raw.githubusercontent.com/lamassu/lamassu-patches/master/logs/bills-log-unencrypted 2>/dev/null
-curl -#o /usr/local/bin/send-server-log https://raw.githubusercontent.com/lamassu/lamassu-patches/master/logs/send-server-log 2>/dev/null
-curl -#o /usr/local/bin/export-cash-in https://raw.githubusercontent.com/lamassu/lamassu-patches/master/logs/export-cash-in 2>/dev/null
-curl -#o /usr/local/bin/export-cash-out https://raw.githubusercontent.com/lamassu/lamassu-patches/master/logs/export-cash-out 2>/dev/null
-curl -#o /usr/local/bin/export-bills https://raw.githubusercontent.com/lamassu/lamassu-patches/master/logs/export-bills 2>/dev/null
-curl -#o /usr/local/bin/cash-out-actions https://raw.githubusercontent.com/lamassu/lamassu-patches/master/logs/cash-out-actions 2>/dev/null
-curl -#o /usr/local/bin/out-actions-unencrypted https://raw.githubusercontent.com/lamassu/lamassu-patches/master/logs/out-actions-unencrypted 2>/dev/null
-curl -#o /usr/local/bin/export-out-actions https://raw.githubusercontent.com/lamassu/lamassu-patches/master/logs/export-out-actions 2>/dev/null
-curl -#o /usr/local/bin/customers-log https://raw.githubusercontent.com/lamassu/lamassu-patches/master/logs/customers-log 2>/dev/null
-curl -#o /usr/local/bin/export-customers https://raw.githubusercontent.com/lamassu/lamassu-patches/master/logs/export-customers 2>/dev/null
-curl -#o /usr/local/bin/sanctions-log https://raw.githubusercontent.com/lamassu/lamassu-patches/master/logs/sanctions-log 2>/dev/null
-curl -#o /usr/local/bin/export-sanctions-log https://raw.githubusercontent.com/lamassu/lamassu-patches/master/logs/export-sanctions-log 2>/dev/null
-curl -#o /usr/local/bin/trades-log https://raw.githubusercontent.com/lamassu/lamassu-patches/master/logs/trades-log 2>/dev/null
-curl -#o /usr/local/bin/trades-log-unencrypted https://raw.githubusercontent.com/lamassu/lamassu-patches/master/logs/trades-log-unencrypted 2>/dev/null
-curl -#o /usr/local/bin/export-trades https://raw.githubusercontent.com/lamassu/lamassu-patches/master/logs/export-trades 2>/dev/null
+curl -#o /usr/local/bin/cash-in-log https://raw.githubusercontent.com/lamassu/lamassu-patches/master/logs/cash-in-log &>/dev/null
+curl -#o /usr/local/bin/cash-out-log https://raw.githubusercontent.com/lamassu/lamassu-patches/master/logs/cash-out-log &>/dev/null
+curl -#o /usr/local/bin/bills-log https://raw.githubusercontent.com/lamassu/lamassu-patches/master/logs/bills-log &>/dev/null
+curl -#o /usr/local/bin/cash-in-unencrypted https://raw.githubusercontent.com/lamassu/lamassu-patches/master/logs/cash-in-unencrypted &>/dev/null
+curl -#o /usr/local/bin/cash-out-unencrypted https://raw.githubusercontent.com/lamassu/lamassu-patches/master/logs/cash-out-unencrypted &>/dev/null
+curl -#o /usr/local/bin/bills-log-unencrypted https://raw.githubusercontent.com/lamassu/lamassu-patches/master/logs/bills-log-unencrypted &>/dev/null
+curl -#o /usr/local/bin/send-server-log https://raw.githubusercontent.com/lamassu/lamassu-patches/master/logs/send-server-log &>/dev/null
+curl -#o /usr/local/bin/export-cash-in https://raw.githubusercontent.com/lamassu/lamassu-patches/master/logs/export-cash-in &>/dev/null
+curl -#o /usr/local/bin/export-cash-out https://raw.githubusercontent.com/lamassu/lamassu-patches/master/logs/export-cash-out &>/dev/null
+curl -#o /usr/local/bin/export-bills https://raw.githubusercontent.com/lamassu/lamassu-patches/master/logs/export-bills &>/dev/null
+curl -#o /usr/local/bin/cash-out-actions https://raw.githubusercontent.com/lamassu/lamassu-patches/master/logs/cash-out-actions &>/dev/null
+curl -#o /usr/local/bin/out-actions-unencrypted https://raw.githubusercontent.com/lamassu/lamassu-patches/master/logs/out-actions-unencrypted &>/dev/null
+curl -#o /usr/local/bin/export-out-actions https://raw.githubusercontent.com/lamassu/lamassu-patches/master/logs/export-out-actions &>/dev/null
+curl -#o /usr/local/bin/customers-log https://raw.githubusercontent.com/lamassu/lamassu-patches/master/logs/customers-log &>/dev/null
+curl -#o /usr/local/bin/export-customers https://raw.githubusercontent.com/lamassu/lamassu-patches/master/logs/export-customers &>/dev/null
+curl -#o /usr/local/bin/sanctions-log https://raw.githubusercontent.com/lamassu/lamassu-patches/master/logs/sanctions-log &>/dev/null
+curl -#o /usr/local/bin/export-sanctions-log https://raw.githubusercontent.com/lamassu/lamassu-patches/master/logs/export-sanctions-log &>/dev/null
+curl -#o /usr/local/bin/trades-log https://raw.githubusercontent.com/lamassu/lamassu-patches/master/logs/trades-log &>/dev/null
+curl -#o /usr/local/bin/trades-log-unencrypted https://raw.githubusercontent.com/lamassu/lamassu-patches/master/logs/trades-log-unencrypted &>/dev/null
+curl -#o /usr/local/bin/export-trades https://raw.githubusercontent.com/lamassu/lamassu-patches/master/logs/export-trades &>/dev/null
 chmod 755 /usr/local/bin/cash-in-log
 chmod 755 /usr/local/bin/cash-out-log
 chmod 755 /usr/local/bin/bills-log
@@ -53,7 +54,7 @@ chmod 755 /usr/local/bin/trades-log
 chmod 755 /usr/local/bin/trades-log-unencrypted
 chmod 755 /usr/local/bin/export-trades
 echo
-echo 'Done! You may now use the following commands, followed by an email address:'
+echo "Done! You may now use the following commands, followed by an email address:"
 echo
 echo "${bold}Encrypted:${normal}" 
 echo
@@ -85,5 +86,5 @@ echo "export-sanctions-log"
 echo "export-trades"
 echo "export-machine-logs"
 echo
-echo 'Refer to the knowledgebase for usage guidelines.'
+echo "Refer to the knowledgebase for usage guidelines."
 echo
