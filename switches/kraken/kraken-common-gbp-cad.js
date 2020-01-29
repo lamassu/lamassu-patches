92a8d47b96fdd22698d156a94e4d0be38319e343
naconner <7396812+naconner@users.noreply.github.com>
2020-01-29 10:43:48 -0500

    Create kraken-common-gbp-cad.js

---
+++ switches/kraken/kraken-common-gbp-cad.js
@@ -0,0 +1,32 @@
+const PAIRS = {
+  BTC: {
+    USD: 'XXBTZUSD',
+    EUR: 'XXBTZEUR',
+    CAD: 'XXBTZCAD',
+    GBP: 'XXBTZGBP'
+  },
+  ETH: {
+    USD: 'XETHZUSD',
+    EUR: 'XETHZEUR',
+    CAD: 'XETHZCAD',
+    GBP: 'XETHZGBP'
+  },
+  ZEC: {
+    USD: 'XZECZUSD',
+    EUR: 'XZECZEUR'
+  },
+  LTC: {
+    USD: 'XLTCZUSD',
+    EUR: 'XLTCZEUR'
+  },
+  DASH: {
+    USD: 'DASHUSD',
+    EUR: 'DASHEUR'
+  },
+  BCH: {
+    USD: 'BCHUSD',
+    EUR: 'BCHEUR'
+  }
+}
+
+module.exports = {PAIRS}
