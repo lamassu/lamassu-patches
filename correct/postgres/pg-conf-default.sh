a9d4f6beda3027a0cf88eda86f7925da5ddf16d6
naconner <7396812+naconner@users.noreply.github.com>
2020-02-11 19:58:52 -0500

    Create pg-conf-default.sh

---
+++ correct/postgres/pg-conf-default.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+echo
+echo "Resetting postgres timezone..."
+supervisorctl stop lamassu-server lamassu-admin-server &>/dev/null
+curl -#o /etc/postgresql/9.5/main/postgresql.conf https://raw.githubusercontent.com/lamassu/lamassu-patches/master/correct/postgres/postgresql.conf &>/dev/null
+/etc/init.d/postgresql restart &>/dev/null
+supervisorctl start lamassu-server lamassu-admin-server &>/dev/null
+echo "Done."
+echo
