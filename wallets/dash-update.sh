74a5b1a9e57b1cc4263b39dc40e6dca2f044f6a5
naconner <7396812+naconner@users.noreply.github.com>
2020-02-19 14:56:58 -0500

    chore: dash v0.15.0

--- wallets/dash-update.sh
+++ wallets/dash-update.sh
@@ -6,20 +6,19 @@ export LOG_FILE=/tmp/dash-update.$(date +"%Y%m%d").log
 echo
 echo "Updating Dash Core. This may take a minute."
 supervisorctl stop dash >> ${LOG_FILE} 2>&1
-
-echo "Downloading..."
-curl -#Lo /tmp/dash.tar.gz https://github.com/dashpay/dash/releases/download/v0.14.0.5/dashcore-0.14.0.5-x86_64-linux-gnu.tar.gz >> ${LOG_FILE} 2>&1
+echo
+echo "Downloading Dash Core v0.15.0..."
+curl -#Lo /tmp/dash.tar.gz https://github.com/dashpay/dash/releases/download/v0.15.0.0/dashcore-0.15.0.0-x86_64-linux-gnu.tar.gz >> ${LOG_FILE} 2>&1
 tar -xzf /tmp/dash.tar.gz -C /tmp/ >> ${LOG_FILE} 2>&1
-
+echo
 echo "Updating..."
 mv /usr/local/bin/dashd /usr/local/bin/dashd-old >> ${LOG_FILE} 2>&1
 mv /usr/local/bin/dash-cli /usr/local/bin/dash-cli-old >> ${LOG_FILE} 2>&1
-mv /usr/local/bin/dash-tx /usr/local/bin/dash-tx-old >> ${LOG_FILE} 2>&1
-mv /usr/local/bin/dash-qt /usr/local/bin/dash-qt-old >> ${LOG_FILE} 2>&1
-cp /tmp/dashcore-0.14.0/bin/* /usr/local/bin/ >> ${LOG_FILE} 2>&1
-rm -r /tmp/dashcore-0.14.0 >> ${LOG_FILE} 2>&1
+cp /tmp/dashcore-0.15.0/bin/dashd /usr/local/bin/dashd >> ${LOG_FILE} 2>&1
+cp /tmp/dashcore-0.15.0/bin/dash-cli /usr/local/bin/dash-cli >> ${LOG_FILE} 2>&1
+rm -r /tmp/dashcore-0.15.0 >> ${LOG_FILE} 2>&1
 rm /tmp/dash.tar.gz >> ${LOG_FILE} 2>&1
 supervisorctl start dash >> ${LOG_FILE} 2>&1
-
+echo
 echo "Dash Core is updated."
 echo
