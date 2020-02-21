65152c047c8a1aa789566e5c1416783b5d5689dd
naconner <7396812+naconner@users.noreply.github.com>
2020-02-21 14:55:09 -0500

    chore: geth v1.9.11

--- wallets/eth-update.sh
+++ wallets/eth-update.sh
@@ -6,16 +6,19 @@ export LOG_FILE=/tmp/eth-update.$(date +"%Y%m%d").log
 echo
 echo "Updating Ethereum. This may take a minute."
 supervisorctl stop ethereum >> ${LOG_FILE} 2>&1
+
 echo
 echo "Downloading..."
-curl -#o /tmp/ethereum.tar.gz https://gethstore.blob.core.windows.net/builds/geth-linux-amd64-1.9.9-01744997.tar.gz >> ${LOG_FILE} 2>&1
+curl -#o /tmp/ethereum.tar.gz https://gethstore.blob.core.windows.net/builds/geth-linux-amd64-1.9.11-6a62fe39.tar.gz >> ${LOG_FILE} 2>&1
 tar -xzf /tmp/ethereum.tar.gz -C /tmp/ >> ${LOG_FILE} 2>&1
+
 echo
 echo "Updating..."
 mv /usr/local/bin/geth /usr/local/bin/geth-old >> ${LOG_FILE} 2>&1
-cp /tmp/geth-linux-amd64-1.9.9-01744997/geth /usr/local/bin/geth >> ${LOG_FILE} 2>&1
-rm -r /tmp/geth-linux-amd64-1.9.9-01744997/ >> ${LOG_FILE} 2>&1
+cp /tmp/geth-linux-amd64-1.9.11-6a62fe39/geth /usr/local/bin/geth >> ${LOG_FILE} 2>&1
+rm -r /tmp/geth-linux-amd64-1.9.11-6a62fe39/ >> ${LOG_FILE} 2>&1
 rm /tmp/ethereum.tar.gz >> ${LOG_FILE} 2>&1
 supervisorctl start ethereum >> ${LOG_FILE} 2>&1
+
 echo
 echo "Ethereum is updated."
