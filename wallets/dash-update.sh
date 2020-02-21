bb4fbf122c9b1733cc42afa9263a9abe578c479d
naconner <7396812+naconner@users.noreply.github.com>
2020-02-21 12:42:00 -0500

    feat: default to PrivateSend

--- wallets/dash-update.sh
+++ wallets/dash-update.sh
@@ -6,10 +6,12 @@ export LOG_FILE=/tmp/dash-update.$(date +"%Y%m%d").log
 echo
 echo "Updating Dash Core. This may take a minute."
 supervisorctl stop dash >> ${LOG_FILE} 2>&1
+
 echo
 echo "Downloading Dash Core v0.15.0..."
 curl -#Lo /tmp/dash.tar.gz https://github.com/dashpay/dash/releases/download/v0.15.0.0/dashcore-0.15.0.0-x86_64-linux-gnu.tar.gz >> ${LOG_FILE} 2>&1
 tar -xzf /tmp/dash.tar.gz -C /tmp/ >> ${LOG_FILE} 2>&1
+
 echo
 echo "Updating..."
 mv /usr/local/bin/dashd /usr/local/bin/dashd-old >> ${LOG_FILE} 2>&1
@@ -18,7 +20,24 @@ cp /tmp/dashcore-0.15.0/bin/dashd /usr/local/bin/dashd >> ${LOG_FILE} 2>&1
 cp /tmp/dashcore-0.15.0/bin/dash-cli /usr/local/bin/dash-cli >> ${LOG_FILE} 2>&1
 rm -r /tmp/dashcore-0.15.0 >> ${LOG_FILE} 2>&1
 rm /tmp/dash.tar.gz >> ${LOG_FILE} 2>&1
-supervisorctl start dash >> ${LOG_FILE} 2>&1
+
 echo
+if grep -xq "enableprivatesend=." /mnt/blockchains/dash/dash.conf
+then
+    echo "enableprivatesend already defined, skipping..."
+else
+    echo "Enabling PrivateSend in config file..."
+    echo -e "enableprivatesend=1" >> /mnt/blockchains/dash/dash.conf
+fi
+if grep -xq "privatesendautostart=." /mnt/blockchains/dash/dash.conf
+then
+    echo "privatesendautostart already defined, skipping..."
+else
+    echo "Setting PrivateSend AutoStart in config file..."
+    echo -e "privatesendautostart=1" >> /mnt/blockchains/dash/dash.conf
+fi
+
+echo
+supervisorctl start dash >> ${LOG_FILE} 2>&1
 echo "Dash Core is updated."
 echo
