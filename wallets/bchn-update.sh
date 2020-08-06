a082b02b60daf6bb1f5450dcefb12ba04eabcd1d
naconner <7396812+naconner@users.noreply.github.com>
2020-08-06 00:06:27 -0400

    Create bchn-update.sh

---
+++ wallets/bchn-update.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+set -e
+
+export LOG_FILE=/tmp/bch-update.$(date +"%Y%m%d").log
+
+echo
+echo "Updating Bitcoin Cash. This may take a minute..."
+supervisorctl stop bitcoincash >> ${LOG_FILE} 2>&1
+echo
+
+echo "Downloading Bitcoin Cash Node v0.21.2..."
+curl -#Lo /tmp/bitcoincash.tar.gz https://github.com/bitcoin-cash-node/bitcoin-cash-node/releases/download/v0.21.2/bitcoin-cash-node-0.21.2-x86_64-linux-gnu.tar.gz >> ${LOG_FILE} 2>&1
+tar -xzf /tmp/bitcoincash.tar.gz -C /tmp/ >> ${LOG_FILE} 2>&1
+echo
+
+echo "Updating wallet..."
+mv /usr/local/bin/bitcoincashd /usr/local/bin/bitcoincashd-old >> ${LOG_FILE} 2>&1
+mv /usr/local/bin/bitcoincash-cli /usr/local/bin/bitcoin-cli-old >> ${LOG_FILE} 2>&1
+cp /tmp/bitcoin-cash-node-0.21.2/bin/bitcoind /usr/local/bin/bitcoincashd >> ${LOG_FILE} 2>&1
+cp /tmp/bitcoin-cash-node-0.21.2/bin/bitcoin-cli /usr/local/bin/bitcoincash-cli >> ${LOG_FILE} 2>&1
+rm -r /tmp/bitcoin-cash-node-0.21.2 >> ${LOG_FILE} 2>&1
+rm /tmp/bitcoincash.tar.gz >> ${LOG_FILE} 2>&1
+echo
+
+echo "Updating wallet plugins..."
+curl -#o $(npm root -g)/lamassu-server/lib/admin/funding.js https://raw.githubusercontent.com/lamassu/lamassu-server/defiant-dingirma/lib/admin/funding.js >> ${LOG_FILE} 2>&1
+curl -#o $(npm root -g)/lamassu-server/lib/plugins/wallet/bitcoincashd/bitcoincashd.js https://raw.githubusercontent.com/lamassu/lamassu-server/defiant-dingirma/lib/plugins/wallet/bitcoincashd/bitcoincashd.js >> ${LOG_FILE} 2>&1
+curl -#o $(npm root -g)/lamassu-server/lib/plugins/wallet/bitcoind/bitcoind.js https://raw.githubusercontent.com/lamassu/lamassu-server/defiant-dingirma/lib/plugins/wallet/bitcoind/bitcoind.js >> ${LOG_FILE} 2>&1
+curl -#o $(npm root -g)/lamassu-server/lib/plugins/wallet/dashd/dashd.js https://raw.githubusercontent.com/lamassu/lamassu-server/defiant-dingirma/lib/plugins/wallet/dashd/dashd.js >> ${LOG_FILE} 2>&1
+curl -#o $(npm root -g)/lamassu-server/lib/plugins/wallet/litecoind/litecoind.js https://raw.githubusercontent.com/lamassu/lamassu-server/defiant-dingirma/lib/plugins/wallet/litecoind/litecoind.js >> ${LOG_FILE} 2>&1
+curl -#o $(npm root -g)/lamassu-server/lib/plugins/wallet/zcashd/zcashd.js https://raw.githubusercontent.com/lamassu/lamassu-server/defiant-dingirma/lib/plugins/wallet/zcashd/zcashd.js >> ${LOG_FILE} 2>&1
+sed -i 's/\<connections\>/maxconnections/g' /mnt/blockchains/bitcoincash/bitcoincash.conf >> ${LOG_FILE} 2>&1
+
+supervisorctl start bitcoincash >> ${LOG_FILE} 2>&1
+supervisorctl restart lamassu-server lamassu-admin-server >> ${LOG_FILE} 2>&1
+echo
+
+echo "Bitcoin Cash is updated."
+echo
