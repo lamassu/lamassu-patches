87cb2523dc72fd732125e97252ea5f7204e18d06
naconner <7396812+naconner@users.noreply.github.com>
2020-08-17 15:37:38 -0400

    Update table-truncate.sh

--- hotfix/table-truncate.sh
+++ hotfix/table-truncate.sh
@@ -1,19 +1,18 @@
 #!/usr/bin/env bash
 
 echo
-echo 'Cleaning up extraneous database entries (pings, event logs, and machine logs)...'
+echo 'Cleaning up extraneous database entries (event logs and machine logs)...'
 echo
 
-supervisorctl stop lamassu-server lamassu-admin-server
+supervisorctl stop lamassu-server lamassu-admin-server &>/dev/null
 su - postgres -c "pg_dump lamassu > /tmp/lamassu-database.bak"
-echo 'Truncating machine_pings...'
-su - postgres -c "psql \"lamassu\" -Atc \"DELETE FROM machine_pings WHERE created < NOW() - INTERVAL '1 hour'\""
 echo 'Truncating machine_events...'
 su - postgres -c "psql \"lamassu\" -Atc \"DELETE FROM machine_events WHERE created < NOW() - INTERVAL '1 hour'\""
 echo 'Truncating server_events...'
 su - postgres -c "psql \"lamassu\" -Atc \"DELETE FROM server_events WHERE created < NOW() - INTERVAL '1 hour'\""
 echo 'Truncating machine logs...'
 su - postgres -c "psql \"lamassu\" -Atc \"DELETE FROM logs WHERE timestamp < NOW() - INTERVAL '1 day'\""
-supervisorctl start lamassu-server lamassu-admin-server
+supervisorctl start lamassu-server lamassu-admin-server &>/dev/null
 echo
 echo 'Done.'
+echo
