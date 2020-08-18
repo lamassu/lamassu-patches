ed11eac8fdf165ddbd61d6520a9c5beb9d3f0a34
naconner <7396812+naconner@users.noreply.github.com>
2020-08-18 13:41:59 -0400

    chore: litecoind v0.18.1

--- wallets/ltc-update.sh
+++ wallets/ltc-update.sh
@@ -8,8 +8,8 @@ echo "Updating Litecoin Core. This may take a minute."
 supervisorctl stop litecoin >> ${LOG_FILE} 2>&1
 echo
 
-echo "Downloading v0.17.1..."
-curl -#o /tmp/litecoin.tar.gz https://download.litecoin.org/litecoin-0.17.1/linux/litecoin-0.17.1-x86_64-linux-gnu.tar.gz >> ${LOG_FILE} 2>&1
+echo "Downloading v0.18.1..."
+curl -#o /tmp/litecoin.tar.gz https://download.litecoin.org/litecoin-0.18.1/linux/litecoin-0.18.1-x86_64-linux-gnu.tar.gz >> ${LOG_FILE} 2>&1
 tar -xzf /tmp/litecoin.tar.gz -C /tmp/ >> ${LOG_FILE} 2>&1
 echo
 
@@ -18,8 +18,8 @@ mv /usr/local/bin/litecoind /usr/local/bin/litecoind-old >> ${LOG_FILE} 2>&1
 mv /usr/local/bin/litecoin-cli /usr/local/bin/litecoin-cli-old >> ${LOG_FILE} 2>&1
 mv /usr/local/bin/litecoin-tx /usr/local/bin/litecoin-tx-old >> ${LOG_FILE} 2>&1
 mv /usr/local/bin/litecoin-qt /usr/local/bin/litecoin-qt-old >> ${LOG_FILE} 2>&1
-cp /tmp/litecoin-0.17.1/bin/* /usr/local/bin/ >> ${LOG_FILE} 2>&1
-rm -r /tmp/litecoin-0.17.1 >> ${LOG_FILE} 2>&1
+cp /tmp/litecoin-0.18.1/bin/* /usr/local/bin/ >> ${LOG_FILE} 2>&1
+rm -r /tmp/litecoin-0.18.1 >> ${LOG_FILE} 2>&1
 rm /tmp/litecoin.tar.gz >> ${LOG_FILE} 2>&1
 echo
 
