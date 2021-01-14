70c75cd368db288e0e37be76fb667aa6e4eec610
naconner <7396812+naconner@users.noreply.github.com>
2021-01-14 12:34:13 -0500

    Update clear-txs-customers.sh

--- scripts/database/clear-txs-customers.sh
+++ scripts/database/clear-txs-customers.sh
@@ -12,8 +12,8 @@ then
   su - postgres -c "psql \"lamassu\" -Atc \"DELETE FROM bills\""
   su - postgres -c "psql \"lamassu\" -Atc \"DELETE FROM cash_out_txs\""
   su - postgres -c "psql \"lamassu\" -Atc \"DELETE FROM cash_out_actions\""
-  su - postgres -c "psql \"lamassu\" -Atc \"DELETE FROM customers\""
   su - postgres -c "psql \"lamassu\" -Atc \"DELETE FROM compliance_overrides\""
+  su - postgres -c "psql \"lamassu\" -Atc \"DELETE FROM customers\""
   su - postgres -c "psql \"lamassu\" -Atc \"INSERT INTO customers (id) values ('47ac1184-8102-11e7-9079-8f13a7117867')\""
   echo
   echo "Done."
