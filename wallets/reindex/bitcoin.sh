8589e548d5193384a044cbec2c6dccc7c10335e5
naconner <7396812+naconner@users.noreply.github.com>
2021-02-05 21:27:10 -0500

    fix: correct btc reindex script

--- wallets/reindex/bitcoin.sh
+++ wallets/reindex/bitcoin.sh
@@ -5,7 +5,7 @@ echo
 echo "Telling bitcoind to reindex the wallet..."
 echo
 curl -#o /etc/supervisor/conf.d/bitcoin.conf https://raw.githubusercontent.com/lamassu/lamassu-patches/master/wallets/reindex/bitcoin-reindex.conf &>/dev/null
-supervisorctl reread bitcoin &>/dev/null
+supervisorctl reread &>/dev/null
 supervisorctl update bitcoin &>/dev/null
 curl -#o /etc/supervisor/conf.d/bitcoin.conf https://raw.githubusercontent.com/lamassu/lamassu-patches/master/wallets/reindex/bitcoin.conf &>/dev/null
 echo "Done. Your latest wallet balance will be displayed after the Bitcoin wallet has fully reindexed."
