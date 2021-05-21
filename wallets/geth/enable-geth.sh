05c3fc7f2ad6f0148df8ca93e58c4b6d05eef712
naconner <7396812+naconner@users.noreply.github.com>
2021-05-21 09:25:46 -0400

    chore: point to release files

--- wallets/geth/enable-geth.sh
+++ wallets/geth/enable-geth.sh
@@ -9,19 +9,20 @@ NEWADMIN=$(npm root -g)/lamassu-server/lib/new-admin/config/accounts.js
 echo
 echo "Enabling Geth option..."
 
-curl -#o $(npm root -g)/lamassu-server/lib/blockchain/common.js https://raw.githubusercontent.com/lamassu/lamassu-server/ecd4a05d90e6e326b4b1ab4bd49ed6ca11984a4b/lib/blockchain/common.js &>/dev/null
-curl -#o $(npm root -g)/lamassu-server/lib/blockchain/ethereum.js https://raw.githubusercontent.com/lamassu/lamassu-server/ecd4a05d90e6e326b4b1ab4bd49ed6ca11984a4b/lib/blockchain/ethereum.js &>/dev/null
-curl -#o $(npm root -g)/lamassu-server/lib/blockchain/install.js https://raw.githubusercontent.com/lamassu/lamassu-server/ecd4a05d90e6e326b4b1ab4bd49ed6ca11984a4b/lib/blockchain/install.js &>/dev/null
-curl -#o $(npm root -g)/lamassu-server/lib/plugins/wallet/geth/base.js https://raw.githubusercontent.com/lamassu/lamassu-server/ecd4a05d90e6e326b4b1ab4bd49ed6ca11984a4b/lib/plugins/wallet/geth/base.js &>/dev/null
+curl -#o $(npm root -g)/lamassu-server/lib/blockchain/common.js https://raw.githubusercontent.com/lamassu/lamassu-server/release-7.5.0/lib/blockchain/common.js &>/dev/null
+curl -#o $(npm root -g)/lamassu-server/lib/blockchain/ethereum.js https://raw.githubusercontent.com/lamassu/lamassu-server/release-7.5.0/lib/blockchain/ethereum.js &>/dev/null
+curl -#o $(npm root -g)/lamassu-server/lib/blockchain/install.js https://raw.githubusercontent.com/lamassu/lamassu-server/release-7.5.0/lib/blockchain/install.js &>/dev/null
+curl -#o $(npm root -g)/lamassu-server/lib/plugins/wallet/geth/base.js https://raw.githubusercontent.com/lamassu/lamassu-server/release-7.5.0/lib/plugins/wallet/geth/base.js &>/dev/null
 
 ## Don't fail if v7.4
 if test -f "$NEWADMIN"; then
-    curl -#o "$NEWADMIN" https://raw.githubusercontent.com/lamassu/lamassu-server/ecd4a05d90e6e326b4b1ab4bd49ed6ca11984a4b/lib/new-admin/config/accounts.js &>/dev/null
+    curl -#o "$NEWADMIN" https://raw.githubusercontent.com/lamassu/lamassu-server/release-7.5.0/lib/new-admin/config/accounts.js &>/dev/null
 fi
 
 ## Ensure any existing config is set to light
 if test -f "$ETHCONF"; then
     curl -#o "$ETHCONF" https://raw.githubusercontent.com/lamassu/lamassu-patches/master/wallets/geth/ethereum.conf &>/dev/null
+    supervisorctl reread &>/dev/null
 fi
 
 supervisorctl restart lamassu-server lamassu-admin-server &>/dev/null
