b592c693a790d55b3f6e9d3bd62e2ff86ff364c6
naconner <7396812+naconner@users.noreply.github.com>
2021-08-01 18:53:05 -0400

    Update clear-txs-customers.sh

--- scripts/database/clear-txs-customers.sh
+++ scripts/database/clear-txs-customers.sh
@@ -4,8 +4,9 @@ set -e
 echo
 echo "WARNING: This script will DELETE all transaction and customer records from your database!"
 echo
-read -p "Are you really sure you want to do this? (y/N) " -n 1 -r
-if [[ $REPLY =~ ^[Yy]$ ]]
+echo "Are you really sure you want to do this? (y/N)"
+read input
+if [[ $input == "Y" || $input == "y" ]]
 then
   echo
   su - postgres -c "psql \"lamassu\" -Atc \"DELETE FROM cash_in_txs\""
@@ -17,5 +18,8 @@ then
   su - postgres -c "psql \"lamassu\" -Atc \"INSERT INTO customers (id) values ('47ac1184-8102-11e7-9079-8f13a7117867')\""
   echo
   echo "Done."
+else
+  echo
+  echo "Cancelled."
 fi
 echo
