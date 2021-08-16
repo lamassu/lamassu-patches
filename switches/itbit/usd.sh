7b76334cf1baf2a6d58651f912dfd1b4c1bb8534
naconner <7396812+naconner@users.noreply.github.com>
2021-08-16 15:32:25 -0400

    Update usd.sh

--- switches/itbit/usd.sh
+++ switches/itbit/usd.sh
@@ -4,10 +4,11 @@ set -e
 echo
 echo "Updating itBit plugin to default to USD trading..."
 curl -#o $(npm root -g)/lamassu-server/lib/plugins/common/itbit.js https://raw.githubusercontent.com/lamassu/lamassu-patches/master/switches/itbit/itbit.js &>/dev/null
+curl -#o $(npm root -g)/lamassu-server/lib/new-admin/config/accounts.js https://raw.githubusercontent.com/lamassu/lamassu-patches/master/switches/itbit/accounts.js &>/dev/null
 
 echo
 echo "Restarting lamassu-server..."
-supervisorctl restart lamassu-server &>/dev/null
+supervisorctl restart lamassu-server lamassu-admin-server &>/dev/null
 echo
 echo "Update complete."
 echo
