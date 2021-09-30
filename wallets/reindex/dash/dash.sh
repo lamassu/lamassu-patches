983997691dec2127cda32c45e38a939c4c5b51ff
naconner <7396812+naconner@users.noreply.github.com>
2021-09-30 11:21:15 -0400

    Update dash.sh

--- wallets/reindex/dash/dash.sh
+++ wallets/reindex/dash/dash.sh
@@ -8,17 +8,19 @@ echo "This may take a minute..."
 
 supervisorctl stop dash &>/dev/null
 
-curl -#Lo /tmp/dash.tar.gz https://github.com/dashpay/dash/releases/download/v0.16.1.1/dashcore-0.16.1.1-x86_64-linux-gnu.tar.gz &>/dev/null
+curl -#Lo /tmp/dash.tar.gz https://github.com/dashpay/dash/releases/download/v0.17.0.3/dashcore-0.17.0.3-x86_64-linux-gnu.tar.gz &>/dev/null
 tar -xzf /tmp/dash.tar.gz -C /tmp/ &>/dev/null
 
-cp /tmp/dashcore-0.16.1/bin/* /usr/local/bin/ &>/dev/null
-rm -r /tmp/dashcore-0.16.1 &>/dev/null
+cp /tmp/dashcore-0.17.0/bin/* /usr/local/bin/ &>/dev/null
+rm -r /tmp/dashcore-0.17.0 &>/dev/null
 rm /tmp/dash.tar.gz &>/dev/null
 
 curl -#o /etc/supervisor/conf.d/dash.conf https://raw.githubusercontent.com/lamassu/lamassu-patches/master/wallets/reindex/dash/dash-reindex.conf &>/dev/null
 
 supervisorctl reread &>/dev/null
 supervisorctl update dash &>/dev/null
+
+sleep 5s
 
 curl -#o /etc/supervisor/conf.d/dash.conf https://raw.githubusercontent.com/lamassu/lamassu-patches/master/wallets/reindex/dash/dash.conf &>/dev/null
 
