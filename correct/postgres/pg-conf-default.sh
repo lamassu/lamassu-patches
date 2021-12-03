bb095eb791ebf6d2dc9406af0527be9f5564289e
naconner <7396812+naconner@users.noreply.github.com>
2021-12-03 16:36:34 -0500

    Update pg-conf-default.sh

--- correct/postgres/pg-conf-default.sh
+++ correct/postgres/pg-conf-default.sh
@@ -2,11 +2,22 @@
 set -e
 
 echo
-echo "Resetting postgres timezone..."
-echo
+echo "Resetting postgres configurations..."
+
 supervisorctl stop lamassu-server lamassu-admin-server &>/dev/null
-curl -#o /etc/postgresql/9.5/main/postgresql.conf https://raw.githubusercontent.com/lamassu/lamassu-patches/master/correct/postgres/postgresql.conf &>/dev/null
+
+chmod 700 /var/lib/postgresql/12/main &>/dev/null
+
+mv /var/lib/postgresql/12/main/postmaster.pid /var/lib/postgresql/12/main/postmaster.pid-old &>/dev/null
+
+curl -#o /etc/postgresql/12/main/pg_hba.conf https://raw.githubusercontent.com/lamassu/lamassu-patches/master/correct/postgres/pg_hba.conf &>/dev/null
+
+curl -#o /etc/postgresql/12/main/postgresql.conf https://raw.githubusercontent.com/lamassu/lamassu-patches/master/correct/postgres/postgresql.conf &>/dev/null
+
 /etc/init.d/postgresql restart &>/dev/null
+
 supervisorctl start lamassu-server lamassu-admin-server &>/dev/null
+
+echo
 echo "Done."
 echo
