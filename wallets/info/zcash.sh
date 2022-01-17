079afee61564fd24d74097b20f6a628c6594638c
naconner <7396812+naconner@users.noreply.github.com>
2022-01-17 13:34:59 -0500

    Update zcash.sh

--- wallets/info/zcash.sh
+++ wallets/info/zcash.sh
@@ -2,7 +2,7 @@
 
 if [ $(dpkg-query -W -f='${Status}' jq 2>/dev/null | grep -c "ok installed") -eq 0 ];
 then
-  DEBIAN_FRONTEND=noninteractive apt-get install -y jq 2>/dev/null;
+  DEBIAN_FRONTEND=noninteractive apt-get install -y jq > /dev/null 2>&1;
 fi
 
 echo
@@ -12,9 +12,9 @@ echo
 echo "Current zcashd release:"
 zcash-cli --version | sed -n '1 p'
 echo
-echo "Current synchronised blocks:"
+echo "Total blocks synchronised:"
 zcash-cli -conf=/mnt/blockchains/zcash/zcash.conf getblockcount
 echo
-echo "Current zcash blockheight:"
+echo "Known Zcash block height:"
 zcash-cli -conf=/mnt/blockchains/zcash/zcash.conf getblockchaininfo | jq -r '.headers'
 echo
