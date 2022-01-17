bcc720aef933ecee6867f0e2b8e4f7d7b59e9ea9
naconner <7396812+naconner@users.noreply.github.com>
2022-01-17 13:58:02 -0500

    feat: add wallet node info scripts (#21)
    * feat: add wallet node info scripts
    
    * fix: get correct daemon versions

---
+++ wallets/info/bitcoin.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+if [ $(dpkg-query -W -f='${Status}' jq 2>/dev/null | grep -c "ok installed") -eq 0 ];
+then
+  DEBIAN_FRONTEND=noninteractive apt-get install -y jq > /dev/null 2>&1;
+fi
+
+echo
+echo "Current bitcoind status:"
+supervisorctl status bitcoin
+echo
+echo "Current bitcoind release:"
+bitcoin-cli --version
+echo
+echo "Total blocks synchronised:"
+bitcoin-cli -conf=/mnt/blockchains/bitcoin/bitcoin.conf getblockcount
+echo
+echo "Known Bitcoin block height:"
+bitcoin-cli -conf=/mnt/blockchains/bitcoin/bitcoin.conf getblockchaininfo | jq -r '.headers'
+echo
