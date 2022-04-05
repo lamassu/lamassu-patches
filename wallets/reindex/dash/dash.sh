32dc973e1b7692bafaee9fe9d3b0623e9166016a
naconner <7396812+naconner@users.noreply.github.com>
2022-04-04 22:13:44 -0400

    fix: use main update script

--- wallets/reindex/dash/dash.sh
+++ wallets/reindex/dash/dash.sh
@@ -6,18 +6,12 @@ echo "Updating dashd and instructing it to reindex the wallet."
 echo
 echo "This may take a minute..."
 
+curl -sS https://raw.githubusercontent.com/lamassu/lamassu-patches/master/wallets/update/dash.sh | bash &>/dev/null
+
 supervisorctl stop dash &>/dev/null
-
-curl -#Lo /tmp/dash.tar.gz https://github.com/dashpay/dash/releases/download/v0.17.0.3/dashcore-0.17.0.3-x86_64-linux-gnu.tar.gz &>/dev/null
-tar -xzf /tmp/dash.tar.gz -C /tmp/ &>/dev/null
-
-cp /tmp/dashcore-0.17.0/bin/* /usr/local/bin/ &>/dev/null
-rm -r /tmp/dashcore-0.17.0 &>/dev/null
-rm /tmp/dash.tar.gz &>/dev/null
 
 curl -#o /etc/supervisor/conf.d/dash.conf https://raw.githubusercontent.com/lamassu/lamassu-patches/master/wallets/reindex/dash/dash-reindex.conf &>/dev/null
 
-supervisorctl reread &>/dev/null
 supervisorctl update dash &>/dev/null
 
 sleep 5s
@@ -26,7 +20,6 @@ curl -#o /etc/supervisor/conf.d/dash.conf https://raw.githubusercontent.com/lama
 
 sleep 10s
 
-supervisorctl reread &>/dev/null
 supervisorctl update dash &>/dev/null
 
 echo
