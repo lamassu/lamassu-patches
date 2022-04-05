0cc7bda19b488789b43e4ccad7edd6785024069b
naconner <7396812+naconner@users.noreply.github.com>
2022-04-04 22:26:32 -0400

    fix: use main update script

--- wallets/reindex/ltc/litecoin.sh
+++ wallets/reindex/ltc/litecoin.sh
@@ -6,25 +6,20 @@ echo "Updating litecoind and instructing it to reindex the wallet."
 echo
 echo "This may take a minute..."
 
+curl -sS https://raw.githubusercontent.com/lamassu/lamassu-patches/master/wallets/update/ltc.sh | bash &>/dev/null
+
 supervisorctl stop litecoin &>/dev/null
-
-curl -#o /tmp/litecoin.tar.gz https://download.litecoin.org/litecoin-0.18.1/linux/litecoin-0.18.1-x86_64-linux-gnu.tar.gz &>/dev/null
-tar -xzf /tmp/litecoin.tar.gz -C /tmp/ &>/dev/null
-
-cp /tmp/litecoin-0.18.1/bin/* /usr/local/bin/ &>/dev/null
-rm -r /tmp/litecoin-0.18.1 &>/dev/null
-rm /tmp/litecoin.tar.gz &>/dev/null
 
 curl -#o /etc/supervisor/conf.d/litecoin.conf https://raw.githubusercontent.com/lamassu/lamassu-patches/master/wallets/reindex/ltc/litecoin-reindex.conf &>/dev/null
 
-supervisorctl reread &>/dev/null
 supervisorctl update litecoin &>/dev/null
+
+sleep 5s
 
 curl -#o /etc/supervisor/conf.d/litecoin.conf https://raw.githubusercontent.com/lamassu/lamassu-patches/master/wallets/reindex/ltc/litecoin.conf &>/dev/null
 
 sleep 10s
 
-supervisorctl reread &>/dev/null
 supervisorctl update litecoin &>/dev/null
 
 echo
