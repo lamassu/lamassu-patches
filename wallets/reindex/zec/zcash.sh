6297405d3d8c977bb9a634ccec2e733b14778494
naconner <7396812+naconner@users.noreply.github.com>
2022-04-04 22:09:31 -0400

    fix: use main update script

--- wallets/reindex/zec/zcash.sh
+++ wallets/reindex/zec/zcash.sh
@@ -6,25 +6,18 @@ echo "Updating zcashd and instructing it to reindex the wallet."
 echo
 echo "This may take a minute..."
 
+curl -sS https://raw.githubusercontent.com/lamassu/lamassu-patches/master/wallets/update/zec.sh | bash &>/dev/null
+
 supervisorctl stop zcash &>/dev/null
-
-curl -#Lo /tmp/zcash.tar.gz https://z.cash/downloads/zcash-4.4.0-linux64-debian-stretch.tar.gz &>/dev/null
-tar -xzf /tmp/zcash.tar.gz -C /tmp/ &>/dev/null
-
-cp /tmp/zcash-4.4.0/bin/* /usr/local/bin/ &>/dev/null
-rm -r /tmp/zcash-4.4.0 &>/dev/null
-rm /tmp/zcash.tar.gz &>/dev/null
 
 curl -#o /etc/supervisor/conf.d/zcash.conf https://raw.githubusercontent.com/lamassu/lamassu-patches/master/wallets/reindex/zec/zcash-reindex.conf &>/dev/null
 
-supervisorctl reread &>/dev/null
 supervisorctl update zcash &>/dev/null
 
 curl -#o /etc/supervisor/conf.d/zcash.conf https://raw.githubusercontent.com/lamassu/lamassu-patches/master/wallets/reindex/zec/zcash.conf &>/dev/null
 
 sleep 10s
 
-supervisorctl reread &>/dev/null
 supervisorctl update zcash &>/dev/null
 
 echo
