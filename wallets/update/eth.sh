ef447d3746beff073ae7d1c540cc5fe000933789
naconner <7396812+naconner@users.noreply.github.com>
2022-04-09 11:45:46 -0400

    chore: use geth v1.10.17

--- wallets/update/eth.sh
+++ wallets/update/eth.sh
@@ -13,7 +13,7 @@ curl -#o /etc/supervisor/conf.d/ethereum.conf https://raw.githubusercontent.com/
 echo
 
 echo "Downloading Geth v1.10.17..."
-curl -#o /tmp/ethereum.tar.gz https://gethstore.blob.core.windows.net/builds/geth-linux-amd64-1.10.17-25c9b49f >> ${LOG_FILE} 2>&1
+curl -#o /tmp/ethereum.tar.gz https://gethstore.blob.core.windows.net/builds/geth-linux-amd64-1.10.17-25c9b49f.tar.gz >> ${LOG_FILE} 2>&1
 tar -xzf /tmp/ethereum.tar.gz -C /tmp/ >> ${LOG_FILE} 2>&1
 echo
 
