5fdb147b6f8789f8783350d175cc71ef46d0942c
naconner <7396812+naconner@users.noreply.github.com>
2022-04-09 11:38:16 -0400

    chore: use geth v1.10.17

--- wallets/update/eth.sh
+++ wallets/update/eth.sh
@@ -12,14 +12,14 @@ echo "Updating Ethereum configuration file..."
 curl -#o /etc/supervisor/conf.d/ethereum.conf https://raw.githubusercontent.com/lamassu/lamassu-patches/master/wallets/conf/ethereum.conf >> ${LOG_FILE} 2>&1
 echo
 
-echo "Downloading Geth v1.10.16..."
-curl -#o /tmp/ethereum.tar.gz https://gethstore.blob.core.windows.net/builds/geth-linux-amd64-1.10.16-20356e57.tar.gz >> ${LOG_FILE} 2>&1
+echo "Downloading Geth v1.10.17..."
+curl -#o /tmp/ethereum.tar.gz https://gethstore.blob.core.windows.net/builds/geth-linux-amd64-1.10.17-25c9b49f >> ${LOG_FILE} 2>&1
 tar -xzf /tmp/ethereum.tar.gz -C /tmp/ >> ${LOG_FILE} 2>&1
 echo
 
 echo "Updating..."
-cp /tmp/geth-linux-amd64-1.10.16-20356e57/geth /usr/local/bin/geth >> ${LOG_FILE} 2>&1
-rm -r /tmp/geth-linux-amd64-1.10.16-20356e57/ >> ${LOG_FILE} 2>&1
+cp /tmp/geth-linux-amd64-1.10.17-25c9b49f/geth /usr/local/bin/geth >> ${LOG_FILE} 2>&1
+rm -r /tmp/geth-linux-amd64-1.10.17-25c9b49f/ >> ${LOG_FILE} 2>&1
 rm /tmp/ethereum.tar.gz >> ${LOG_FILE} 2>&1
 echo
 
