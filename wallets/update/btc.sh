3bd036186526f32a680e69b35305f5c491a0dc3e
naconner <7396812+naconner@users.noreply.github.com>
2022-05-04 19:17:54 -0400

    chore: use bitcoin v23.0, default to bech32

--- wallets/update/btc.sh
+++ wallets/update/btc.sh
@@ -8,15 +8,23 @@ echo "Updating Bitcoin Core. This may take a minute."
 supervisorctl stop bitcoin >> ${LOG_FILE} 2>&1
 echo
 
-echo "Downloading v22.0..."
-curl -#o /tmp/bitcoin.tar.gz https://bitcoincore.org/bin/bitcoin-core-22.0/bitcoin-22.0-x86_64-linux-gnu.tar.gz >> ${LOG_FILE} 2>&1
+echo "Downloading v23.0..."
+curl -#o /tmp/bitcoin.tar.gz https://bitcoincore.org/bin/bitcoin-core-23.0/bitcoin-23.0-x86_64-linux-gnu.tar.gz >> ${LOG_FILE} 2>&1
 tar -xzf /tmp/bitcoin.tar.gz -C /tmp/ >> ${LOG_FILE} 2>&1
 echo
 
 echo "Updating wallet..."
-cp /tmp/bitcoin-22.0/bin/* /usr/local/bin/ >> ${LOG_FILE} 2>&1
-rm -r /tmp/bitcoin-22.0 >> ${LOG_FILE} 2>&1
+cp /tmp/bitcoin-23.0/bin/* /usr/local/bin/ >> ${LOG_FILE} 2>&1
+rm -r /tmp/bitcoin-23.0 >> ${LOG_FILE} 2>&1
 rm /tmp/bitcoin.tar.gz >> ${LOG_FILE} 2>&1
+echo
+
+if grep -q "addresstype=p2sh-segwit" /mnt/blockchains/bitcoin/bitcoin.conf
+then
+    sed -i 's/addresstype=p2sh-segwit/addresstype=bech32/g' /mnt/blockchains/bitcoin/bitcoin.conf
+else
+    echo "bech32 receiving addresses already defined, skipping..."
+fi
 echo
 
 if grep -q "changetype=" /mnt/blockchains/bitcoin/bitcoin.conf
