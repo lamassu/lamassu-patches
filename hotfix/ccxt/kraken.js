20e5a04b50ec20cf2f932c1d4c18d64aa29fff7b
naconner <7396812+naconner@users.noreply.github.com>
2022-05-24 17:25:02 -0400

    Create kraken.js

---
+++ hotfix/ccxt/kraken.js
@@ -0,0 +1,29 @@
+const _ = require('lodash/fp')
+
+const { ORDER_TYPES } = require('./consts')
+const { COINS } = require('@lamassu/coins')
+
+const ORDER_TYPE = ORDER_TYPES.MARKET
+const { BTC, BCH, DASH, ETH, LTC, ZEC, XMR } = COINS
+const CRYPTO = [BTC, ETH, LTC, DASH, ZEC, BCH, XMR]
+const FIAT = ['USD', 'EUR']
+const AMOUNT_PRECISION = 6
+const REQUIRED_CONFIG_FIELDS = ['apiKey', 'privateKey']
+const USER_REF = 'userref'
+
+const loadConfig = (account) => {
+  const mapper = {
+    'privateKey': 'secret'
+  }
+  const mapped = _.mapKeys(key => mapper[key] ? mapper[key] : key)(account)
+
+  return {
+    ...mapped,
+    timeout: 3000,
+    nonce: function () { return this.microseconds() }
+  }
+}
+
+const loadOptions = () => ({ expiretm: '+60' })
+
+module.exports = { USER_REF, loadOptions, loadConfig, REQUIRED_CONFIG_FIELDS, CRYPTO, FIAT, ORDER_TYPE, AMOUNT_PRECISION }
