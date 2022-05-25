3d2b5ae5499c342d0c50599d42c0ecabf0822aa6
naconner <7396812+naconner@users.noreply.github.com>
2022-05-25 06:41:18 -0400

    Update ccxt-patch.sh

--- hotfix/ccxt/ccxt-patch.sh
+++ hotfix/ccxt/ccxt-patch.sh
@@ -2,15 +2,21 @@
 set -e
 
 echo
+
 echo "Patching to default to the EUR market..."
-echo
 
 cd $(npm root -g)/lamassu-server/lib/plugins/common/
-
 curl -#o ccxt.js https://raw.githubusercontent.com/lamassu/lamassu-patches/master/hotfix/ccxt/ccxt.js &>/dev/null
+echo
+
+echo "Patching to increase Kraken API request nonce precision..."
+
+cd $(npm root -g)/lamassu-server/lib/plugins/exchange/
+curl -#o kraken.js https://raw.githubusercontent.com/lamassu/lamassu-patches/master/hotfix/ccxt/kraken.js &>/dev/null
+echo
 
 sleep 2s
 supervisorctl restart lamassu-server lamassu-admin-server &>/dev/null
 
-echo "Trader file patched."
+echo "Done."
 echo
