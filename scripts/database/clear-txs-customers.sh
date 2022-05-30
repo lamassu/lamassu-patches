569786a1fc9f927263764d31773fe3d7aa826259
naconner <7396812+naconner@users.noreply.github.com>
2022-05-30 10:10:02 -0400

    fix: use cascade

--- scripts/database/clear-txs-customers.sh
+++ scripts/database/clear-txs-customers.sh
@@ -2,19 +2,19 @@
 set -e
 
 echo
-echo "WARNING: This script will DELETE all transaction and customer records from your database!"
+echo "WARNING: This script will DELETE ALL transaction and ALL customer records from your database!"
 echo
 echo "Are you really sure you want to do this? (y/N)"
 read input
 if [[ $input == "Y" || $input == "y" ]]
 then
   echo
-  su - postgres -c "psql \"lamassu\" -Atc \"DELETE FROM cash_in_txs\""
-  su - postgres -c "psql \"lamassu\" -Atc \"DELETE FROM bills\""
-  su - postgres -c "psql \"lamassu\" -Atc \"DELETE FROM cash_out_txs\""
-  su - postgres -c "psql \"lamassu\" -Atc \"DELETE FROM cash_out_actions\""
-  su - postgres -c "psql \"lamassu\" -Atc \"DELETE FROM compliance_overrides\""
-  su - postgres -c "psql \"lamassu\" -Atc \"DELETE FROM customers\""
+  su - postgres -c "psql \"lamassu\" -Atc \"TRUNCATE cash_in_txs CASCADE\""
+  su - postgres -c "psql \"lamassu\" -Atc \"TRUNCATE bills CASCADE\""
+  su - postgres -c "psql \"lamassu\" -Atc \"TRUNCATE cash_out_txs CASCADE\""
+  su - postgres -c "psql \"lamassu\" -Atc \"TRUNCATE cash_out_actions CASCADE\""
+  su - postgres -c "psql \"lamassu\" -Atc \"TRUNCATE compliance_overrides CASCADE\""
+  su - postgres -c "psql \"lamassu\" -Atc \"TRUNCATE customers CASCADE\""
   su - postgres -c "psql \"lamassu\" -Atc \"INSERT INTO customers (id) values ('47ac1184-8102-11e7-9079-8f13a7117867')\""
   echo
   echo "Done."
