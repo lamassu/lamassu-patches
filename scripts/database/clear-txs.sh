5b1e3c0af2119c3e4230259b2cf0bae464f4c6d9
Lamatos <93525751+Lamatos@users.noreply.github.com>
2022-06-17 18:33:50 +0100

    Update clear-txs.sh (#27)

--- scripts/database/clear-txs.sh
+++ scripts/database/clear-txs.sh
@@ -2,13 +2,21 @@
 set -e
 
 echo
-su - postgres -c "psql \"lamassu\" -Atc \"DELETE FROM cash_in_txs\""
-su - postgres -c "psql \"lamassu\" -Atc \"DELETE FROM bills\""
-su - postgres -c "psql \"lamassu\" -Atc \"DELETE FROM cash_out_txs\""
-su - postgres -c "psql \"lamassu\" -Atc \"DELETE FROM cash_out_actions\""
-su - postgres -c "psql \"lamassu\" -Atc \"DELETE FROM compliance_overrides\""
-su - postgres -c "psql \"lamassu\" -Atc \"DELETE FROM customers\""
-su - postgres -c "psql \"lamassu\" -Atc \"INSERT INTO customers (id) values ('47ac1184-8102-11e7-9079-8f13a7117867')\""
-
+echo "WARNING: This script will DELETE ALL transaction records from your database!"
 echo
-echo "Done."
+echo "Are you really sure you want to do this? (y/N)"
+read input
+if [[ $input == "Y" || $input == "y" ]]
+then
+  echo
+  su - postgres -c "psql \"lamassu\" -Atc \"TRUNCATE cash_in_txs CASCADE\""
+  su - postgres -c "psql \"lamassu\" -Atc \"TRUNCATE bills CASCADE\""
+  su - postgres -c "psql \"lamassu\" -Atc \"TRUNCATE cash_out_txs CASCADE\""
+  su - postgres -c "psql \"lamassu\" -Atc \"TRUNCATE cash_out_actions CASCADE\""
+  echo
+  echo "Done."
+else
+  echo
+  echo "Cancelled."
+fi
+echo
