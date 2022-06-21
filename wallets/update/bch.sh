5eb5ff96e7e1bb07aa7c4e95b45c640d82aa3b78
naconner <7396812+naconner@users.noreply.github.com>
2022-06-21 17:20:30 -0400

    chore: use bchd v24.1.0

--- wallets/update/bch.sh
+++ wallets/update/bch.sh
@@ -8,16 +8,25 @@ echo "Updating Bitcoin Cash. This may take a minute..."
 supervisorctl stop bitcoincash >> ${LOG_FILE} 2>&1
 echo
 
-echo "Downloading Bitcoin Cash Node v24.0.0..."
-curl -#Lo /tmp/bitcoincash.tar.gz https://github.com/bitcoin-cash-node/bitcoin-cash-node/releases/download/v24.0.0/bitcoin-cash-node-24.0.0-x86_64-linux-gnu.tar.gz >> ${LOG_FILE} 2>&1
+echo "Downloading Bitcoin Cash Node v24.1.0..."
+curl -#Lo /tmp/bitcoincash.tar.gz https://github.com/bitcoin-cash-node/bitcoin-cash-node/releases/download/v24.1.0/bitcoin-cash-node-24.1.0-x86_64-linux-gnu.tar.gz >> ${LOG_FILE} 2>&1
 tar -xzf /tmp/bitcoincash.tar.gz -C /tmp/ >> ${LOG_FILE} 2>&1
 echo
 
 echo "Updating wallet..."
-cp /tmp/bitcoin-cash-node-24.0.0/bin/bitcoind /usr/local/bin/bitcoincashd >> ${LOG_FILE} 2>&1
-cp /tmp/bitcoin-cash-node-24.0.0/bin/bitcoin-cli /usr/local/bin/bitcoincash-cli >> ${LOG_FILE} 2>&1
-rm -r /tmp/bitcoin-cash-node-24.0.0 >> ${LOG_FILE} 2>&1
+cp /tmp/bitcoin-cash-node-24.1.0/bin/bitcoind /usr/local/bin/bitcoincashd >> ${LOG_FILE} 2>&1
+cp /tmp/bitcoin-cash-node-24.1.0/bin/bitcoin-cli /usr/local/bin/bitcoincash-cli >> ${LOG_FILE} 2>&1
+rm -r /tmp/bitcoin-cash-node-24.1.0 >> ${LOG_FILE} 2>&1
 rm /tmp/bitcoincash.tar.gz >> ${LOG_FILE} 2>&1
+echo
+
+if grep -q "listenonion=" /mnt/blockchains/bitcoincash/bitcoincash.conf
+then
+    echo "listenonion already defined, skipping..."
+else
+    echo "Setting 'listenonion=0' in config file..."
+    echo -e "\nlistenonion=0" >> /mnt/blockchains/bitcoincash/bitcoincash.conf
+fi
 echo
 
 echo "Starting wallet..."
