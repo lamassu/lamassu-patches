b87620f58d6d633048689604b742da81459e54ed
naconner <7396812+naconner@users.noreply.github.com>
2022-07-05 13:28:51 -0400

    fix: logging

--- wallets/repair/bch-repair.sh
+++ wallets/repair/bch-repair.sh
@@ -15,26 +15,26 @@ rsync -a --delete empty-$d/ chainstate/
 rsync -a --delete empty-$d/ blocks/
 
 echo 'Updating Bitcoin Cash...'
-curl -#o /tmp/bitcoincash.tar.gz https://github.com/bitcoin-cash-node/bitcoin-cash-node/releases/download/v24.1.0/bitcoin-cash-node-24.1.0-x86_64-linux-gnu.tar.gz >> ${LOG_FILE} 2>&1
-tar -xzf /tmp/bitcoincash.tar.gz -C /tmp/ >> ${LOG_FILE} 2>&1
-cp /tmp/bitcoin-cash-node-24.1.0/bin/bitcoind /usr/local/bin/bitcoincashd >> ${LOG_FILE} 2>&1
-cp /tmp/bitcoin-cash-node-24.1.0/bin/bitcoin-cli /usr/local/bin/bitcoincash-cli >> ${LOG_FILE} 2>&1
-rm -r /tmp/bitcoin-cash-node-24.1.0 >> ${LOG_FILE} 2>&1
-rm /tmp/bitcoincash.tar.gz >> ${LOG_FILE} 2>&1
+curl -#o /tmp/bitcoincash.tar.gz https://github.com/bitcoin-cash-node/bitcoin-cash-node/releases/download/v24.1.0/bitcoin-cash-node-24.1.0-x86_64-linux-gnu.tar.gz &>/dev/null
+tar -xzf /tmp/bitcoincash.tar.gz -C /tmp/ &>/dev/null
+cp /tmp/bitcoin-cash-node-24.1.0/bin/bitcoind /usr/local/bin/bitcoincashd &>/dev/null
+cp /tmp/bitcoin-cash-node-24.1.0/bin/bitcoin-cli /usr/local/bin/bitcoincash-cli &>/dev/null
+rm -r /tmp/bitcoin-cash-node-24.1.0 &>/dev/null
+rm /tmp/bitcoincash.tar.gz &>/dev/null
 
 echo 'Clearing Bitcoin Cash logs...'
 rm /var/log/supervisor/bitcoincash.err.log
 rm /var/log/supervisor/bitcoincash.out.log
 
 echo "Resetting Bitcoin Cash configurations..."
-sed -i 's/\<connections\>/maxconnections/g' /mnt/blockchains/bitcoincash/bitcoincash.conf >> ${LOG_FILE} 2>&1
-curl -#o /etc/supervisor/conf.d/bitcoin.conf https://raw.githubusercontent.com/lamassu/lamassu-patches/master/wallets/repair/bitcoincash.conf >> ${LOG_FILE} 2>&1
-supervisorctl reread bitcoincash >> ${LOG_FILE} 2>&1
-supervisorctl update bitcoincash >> ${LOG_FILE} 2>&1
+sed -i 's/\<connections\>/maxconnections/g' /mnt/blockchains/bitcoincash/bitcoincash.conf &>/dev/null
+curl -#o /etc/supervisor/conf.d/bitcoin.conf https://raw.githubusercontent.com/lamassu/lamassu-patches/master/wallets/repair/bitcoincash.conf &>/dev/null
+supervisorctl reread bitcoincash &>/dev/null
+supervisorctl update bitcoincash &>/dev/null
 
 echo 'Starting Bitcoin Cash...'
-supervisorctl start bitcoincash >> ${LOG_FILE} 2>&1
-supervisorctl restart lamassu-server lamassu-admin-server >> ${LOG_FILE} 2>&1
+supervisorctl start bitcoincash &>/dev/null
+supervisorctl restart lamassu-server lamassu-admin-server &>/dev/null
 
 echo 'Done!'
 echo
