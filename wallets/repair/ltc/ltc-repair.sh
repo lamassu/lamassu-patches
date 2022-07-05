acf64c9ae6c07cda20ae48373a1a04166c11a931
naconner <7396812+naconner@users.noreply.github.com>
2022-07-05 13:54:13 -0400

    Update ltc-repair.sh

--- wallets/repair/ltc/ltc-repair.sh
+++ wallets/repair/ltc/ltc-repair.sh
@@ -15,11 +15,11 @@ rsync -a --delete empty-$d/ chainstate/
 rsync -a --delete empty-$d/ blocks/
 
 echo 'Updating Litecoin...'
-curl -#Lo /tmp/litecoin.tar.gz https://github.com/litecoin-project/litecoin/releases/download/v0.21.2.1/litecoin-0.21.2.1-x86_64-linux-gnu.tar.gz >> ${LOG_FILE} 2>&1
-tar -xzf /tmp/litecoin.tar.gz -C /tmp/ >> ${LOG_FILE} 2>&1
-cp /tmp/litecoin-0.21.2.1/bin/* /usr/local/bin/ >> ${LOG_FILE} 2>&1
-rm -r /tmp/litecoin-0.21.2.1 >> ${LOG_FILE} 2>&1
-rm /tmp/litecoin.tar.gz >> ${LOG_FILE} 2>&1
+curl -#Lo /tmp/litecoin.tar.gz https://github.com/litecoin-project/litecoin/releases/download/v0.21.2.1/litecoin-0.21.2.1-x86_64-linux-gnu.tar.gz &>/dev/null
+tar -xzf /tmp/litecoin.tar.gz -C /tmp/ &>/dev/null
+cp /tmp/litecoin-0.21.2.1/bin/* /usr/local/bin/ &>/dev/null
+rm -r /tmp/litecoin-0.21.2.1 &>/dev/null
+rm /tmp/litecoin.tar.gz &>/dev/null
 
 echo 'Clearing Litecoin logs...'
 set +e
@@ -28,13 +28,13 @@ mv /var/log/supervisor/litecoin.out.log /var/log/supervisor/litecoin-old.out.log
 set -e
 
 echo "Resetting Litecoin configurations..."
-curl -#o /etc/supervisor/conf.d/litecoin.conf https://raw.githubusercontent.com/lamassu/lamassu-patches/master/wallets/repair/ltc/litecoin.conf >> ${LOG_FILE} 2>&1
-supervisorctl reread >> ${LOG_FILE} 2>&1
-supervisorctl update litecoin >> ${LOG_FILE} 2>&1
+curl -#o /etc/supervisor/conf.d/litecoin.conf https://raw.githubusercontent.com/lamassu/lamassu-patches/master/wallets/repair/ltc/litecoin.conf &>/dev/null
+supervisorctl reread &>/dev/null
+supervisorctl update litecoin &>/dev/null
 
 echo 'Starting Litecoin...'
-supervisorctl start litecoin >> ${LOG_FILE} 2>&1
-supervisorctl restart lamassu-server lamassu-admin-server >> ${LOG_FILE} 2>&1
+supervisorctl start litecoin &>/dev/null
+supervisorctl restart lamassu-server lamassu-admin-server &>/dev/null
 
 echo 'Done!'
 echo
