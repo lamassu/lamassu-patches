da165b1e405505dee255973d7586f09cd1bfe6ea
naconner <7396812+naconner@users.noreply.github.com>
2022-07-15 08:35:49 -0400

    Create default-aud.sh

---
+++ switches/trading/default-aud.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -e
+
+echo
+echo "Updating trading plugin to default to AUD..."
+
+supervisorctl stop lamassu-server &>/dev/null
+sed -i "s/'EUR'/'AUD'/g" $(npm root -g)/lamassu-server/lib/plugins/common/ccxt.js &>/dev/null
+supervisorctl start lamassu-server &>/dev/null
+
+echo
+echo "Done."
+echo
+echo "Note: If you update to a new server release, this script will need to be run again (until a market selector is included within the admin)."
+echo
