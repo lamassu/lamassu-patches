8fd651f9c4fce767aad5083c6b817a43814e6d82
naconner <7396812+naconner@users.noreply.github.com>
2022-07-21 10:03:56 -0400

    Create currency-peg.sh

---
+++ hotfix/ticker/currency-peg.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+echo
+echo "Adding ZAD : ZAR currency peg..."
+
+supervisorctl stop lamassu-server lamassu-admin-server &>/dev/null
+
+curl -o $(npm root -g)/lamassu-server/lib/ticker.js https://raw.githubusercontent.com/lamassu/lamassu-server/cc0d5e01c429659880fef9d205e6cc449478a910/lib/ticker.js &>/dev/null
+
+supervisorctl start lamassu-server lamassu-admin-server &>/dev/null
+
+echo
+echo "Done!"
+echo
