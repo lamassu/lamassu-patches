4aa5ce1a9b4db6990e129f8076e4334b43792e3e
naconner <7396812+naconner@users.noreply.github.com>
2022-08-09 17:33:58 -0400

    Create monero.sh

---
+++ wallets/disable/monero.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -e
+
+d=$(date -u "+%s")
+
+echo
+echo 'Stopping Monero...'
+supervisorctl stop monero monero-wallet &>/dev/null
+
+echo
+echo 'Clearing the Monero database...'
+cd /mnt/blockchains/monero
+mkdir empty-$d
+rsync -a --delete empty-$d/ lmdb/
+
+echo
+echo "Disabling Monero..."
+cd /etc/supervisor/conf.d
+mkdir disabled
+mv monero.conf disabled/
+mv monero-wallet.conf disabled/
+supervisorctl reread &>/dev/null
+supervisorctl update monero monero-wallet &>/dev/null
+
+echo
+echo "Done."
+echo
