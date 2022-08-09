863c8cee38eba96ad9274aa9a88055ad219ee76c
naconner <7396812+naconner@users.noreply.github.com>
2022-08-09 17:43:06 -0400

    Create zcash.sh

---
+++ wallets/disable/zcash.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -e
+
+d=$(date -u "+%s")
+
+echo
+echo "Stopping Zcash..."
+supervisorctl stop zcash &>/dev/null
+
+echo
+echo "Clearing the Zcash database..."
+cd /mnt/blockchains/zcash
+mkdir empty-$d
+rsync -a --delete empty-$d/ blocks/
+
+echo
+echo "Disabling Zcash..."
+cd /etc/supervisor/conf.d
+mkdir disabled
+mv zcash.conf disabled/
+supervisorctl reread &>/dev/null
+supervisorctl update zcash &>/dev/null
+
+echo
+echo "Done."
+echo
