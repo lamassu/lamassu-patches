8a9b7e608d49350cabd2506e1528331c93775f53
CrypticaScriptura <7396812+CrypticaScriptura@users.noreply.github.com>
2022-08-13 09:36:52 -0400

    chore: use monerod v0.18.1.0

--- wallets/update/xmr.sh
+++ wallets/update/xmr.sh
@@ -8,15 +8,15 @@ echo "Updating your Monero wallet. This may take a minute."
 supervisorctl stop monero monero-wallet >> ${LOG_FILE} 2>&1
 echo
 
-echo "Downloading Monero v0.18.0.0..."
-curl -#Lo /tmp/monero.tar.bz2 https://downloads.getmonero.org/cli/monero-linux-x64-v0.18.0.0.tar.bz2 >> ${LOG_FILE} 2>&1
+echo "Downloading Monero v0.18.1.0..."
+curl -#Lo /tmp/monero.tar.bz2 https://downloads.getmonero.org/cli/monero-linux-x64-v0.18.1.0.tar.bz2 >> ${LOG_FILE} 2>&1
 tar -xf /tmp/monero.tar.bz2 -C /tmp/ >> ${LOG_FILE} 2>&1
 echo
 
 echo "Updating wallet..."
-cp /tmp/monero-x86_64-linux-gnu-v0.18.0.0/monerod /usr/local/bin/ >> ${LOG_FILE} 2>&1
-cp /tmp/monero-x86_64-linux-gnu-v0.18.0.0/monero-wallet-rpc /usr/local/bin/ >> ${LOG_FILE} 2>&1
-rm -r /tmp/monero-x86_64-linux-gnu-v0.18.0.0 >> ${LOG_FILE} 2>&1
+cp /tmp/monero-x86_64-linux-gnu-v0.18.1.0/monerod /usr/local/bin/ >> ${LOG_FILE} 2>&1
+cp /tmp/monero-x86_64-linux-gnu-v0.18.1.0/monero-wallet-rpc /usr/local/bin/ >> ${LOG_FILE} 2>&1
+rm -r /tmp/monero-x86_64-linux-gnu-v0.18.1.0 >> ${LOG_FILE} 2>&1
 rm /tmp/monero.tar.bz2 >> ${LOG_FILE} 2>&1
 echo
 
