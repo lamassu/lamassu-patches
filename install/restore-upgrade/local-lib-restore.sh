5166858d37ccbc6494c665c3b1cbef8a6dd2ad29
CrypticaScriptura <7396812+CrypticaScriptura@users.noreply.github.com>
2022-10-30 18:44:17 -0400

    Create local-lib-restore.sh

---
+++ install/restore-upgrade/local-lib-restore.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+echo
+echo "Making node_modules folder..."
+
+mkdir -p /usr/local/lib/node_modules
+
+echo
+echo "Restoring npm..."
+
+curl -sS https://ssubucket.ams3.digitaloceanspaces.com/server-rescue/npm.tar.xz | tar -xJ -C /usr/local/lib/node_modules/
+
+echo
+echo "Restoring corepack..."
+
+curl -sS https://ssubucket.ams3.digitaloceanspaces.com/server-rescue/corepack.tar.xz | tar -xJ -C /usr/local/lib/node_modules/
+
+echo
+echo "Restoring lamassu-server..."
+
+curl -sS https://ssubucket.ams3.digitaloceanspaces.com/server-rescue/lamassuserver.tar.xz | tar -xJ -C /usr/local/lib/node_modules/
+
+echo
+echo "Removing update lock..."
+
+rmdir /var/lock/lamassu-update &>/dev/null
+
+echo "Upgrading. Please wait up to 20 minutes..."
+
+curl -sS https://raw.githubusercontent.com/lamassu/lamassu-install/forsaken-fiat/upgrade-ls | bash
