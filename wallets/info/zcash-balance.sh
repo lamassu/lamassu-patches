d7612386038792b619032f30618bff545080dd17
CrypticaScriptura <7396812+CrypticaScriptura@users.noreply.github.com>
2023-03-10 14:47:59 -0500

    fix: add deprecated RPC commands

--- wallets/info/zcash-balance.sh
+++ wallets/info/zcash-balance.sh
@@ -5,6 +5,41 @@ then
   DEBIAN_FRONTEND=noninteractive apt-get install -y jq > /dev/null 2>&1;
 fi
 
+supervisorctl stop zcash
+
+if grep -q "allowdeprecated=getnewaddress" /mnt/blockchains/zcash/zcash.conf
+then
+    echo "allowdeprecated=getnewaddress already defined, skipping..."
+else
+    echo "Setting 'allowdeprecated=getnewaddress' in config file..."
+    echo -e "\nallowdeprecated=getnewaddress" >> /mnt/blockchains/zcash/zcash.conf
+fi
+echo
+
+if grep -q "allowdeprecated=z_listaddresses" /mnt/blockchains/zcash/zcash.conf
+then
+    echo "allowdeprecated=z_listaddresses already defined, skipping..."
+else
+    echo "Setting 'allowdeprecated=z_listaddresses' in config file..."
+    echo -e "\nallowdeprecated=z_listaddresses" >> /mnt/blockchains/zcash/zcash.conf
+fi
+echo
+
+if grep -q "allowdeprecated=z_getbalance" /mnt/blockchains/zcash/zcash.conf
+then
+    echo "allowdeprecated=z_getbalance already defined, skipping..."
+else
+    echo "Setting 'allowdeprecated=z_getbalance' in config file..."
+    echo -e "\nallowdeprecated=z_getbalance" >> /mnt/blockchains/zcash/zcash.conf
+fi
+echo
+
+supervisorctl start zcash
+
+echo
+echo "Waiting for zcashd to start. This will take 20 seconds..."
+sleep 20s
+
 echo
 echo "Here are your addresses and balances:"
 echo
