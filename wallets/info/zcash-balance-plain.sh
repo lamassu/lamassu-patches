9b0b8346de070b9d137ae2d195a85c8da3f68ffe
CrypticaScriptura <7396812+CrypticaScriptura@users.noreply.github.com>
2023-03-10 17:36:24 -0500

    Create zcash-balance-plain.sh

---
+++ wallets/info/zcash-balance-plain.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+echo
+echo "Here are your addresses and balances:"
+echo
+paste <(zcash-cli -conf=/mnt/blockchains/zcash/zcash.conf z_listaddresses |  jq -r "to_entries|map(\"\(.value|tostring)\")|.[]") <(zcash-cli -conf=/mnt/blockchains/zcash/zcash.conf z_listaddresses |  jq -r "to_entries|map(\"\(.value|tostring)\")|.[]" | xargs -n1 zcash-cli -conf=/mnt/blockchains/zcash/zcash.conf z_getbalance) | column -s $'\t' -t
+echo
