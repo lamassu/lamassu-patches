ba58705f41a5661bc63a8a44131c45e04e816d3b
CrypticaScriptura <7396812+CrypticaScriptura@users.noreply.github.com>
2023-03-15 09:11:11 -0400

    Update zcash-balance-plain.sh

--- wallets/info/zcash-balance-plain.sh
+++ wallets/info/zcash-balance-plain.sh
@@ -3,5 +3,5 @@
 echo
 echo "Here are your addresses and balances:"
 echo
-paste <(zcash-cli -conf=/mnt/blockchains/zcash/zcash.conf z_listaddresses |  jq -r "to_entries|map(\"\(.value|tostring)\")|.[]") <(zcash-cli -conf=/mnt/blockchains/zcash/zcash.conf z_listaddresses |  jq -r "to_entries|map(\"\(.value|tostring)\")|.[]" | xargs -n1 zcash-cli -conf=/mnt/blockchains/zcash/zcash.conf z_getbalance) | column -s $'\t' -t
+paste <(zcash-cli -conf=/mnt/blockchains/zcash/zcash.conf z_listaddresses |  jq -r "to_entries|map(\"\(.value|tostring)\")|.[]") <(zcash-cli -conf=/mnt/blockchains/zcash/zcash.conf z_listaddresses |  jq -r "to_entries|map(\"\(.value|tostring)\")|.[]" | xargs -n1 zcash-cli -conf=/mnt/blockchains/zcash/zcash.conf getbalance) | column -s $'\t' -t
 echo
