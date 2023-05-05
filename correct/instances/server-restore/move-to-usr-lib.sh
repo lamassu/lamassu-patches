84e218c52f58d4b973b4bbf68ecf6cd46ee6c32e
CrypticaScriptura <7396812+CrypticaScriptura@users.noreply.github.com>
2023-05-05 08:59:15 -0400

    Create move-to-usr-lib.sh

---
+++ correct/instances/server-restore/move-to-usr-lib.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+echo
+echo "Moving lamassu-server to new location..."
+echo
+supervisorctl stop lamassu-server lamassu-admin-server
+mkdir -p /usr/lib/node_modules
+mv /usr/lib/node_modules/lamassu-server /usr/lib/node_modules/lamassu-server-old
+mv /usr/local/lib/node_modules/lamassu-server /usr/lib/node_modules/lamassu-server
+rm /usr/local/bin/lamassu-server
+rm /usr/local/bin/lamassu-admin-server
+ln -s /usr/lib/node_modules/lamassu-server/bin/lamassu-server /usr/bin/lamassu-server
+ln -s /usr/lib/node_modules/lamassu-server/bin/lamassu-admin-server /usr/bin/lamassu-admin-server
+curl -o /etc/supervisor/conf.d/lamassu-server.conf https://raw.githubusercontent.com/lamassu/lamassu-patches/master/correct/instances/server-restore/lamassu-server.conf
+curl -o /etc/supervisor/conf.d/lamassu-admin-server.conf https://raw.githubusercontent.com/lamassu/lamassu-patches/master/correct/instances/server-restore/lamassu-admin-server.conf
+supervisorctl reread
+supervisorctl update lamassu-server lamassu-admin-server
+echo
+echo "Done."
