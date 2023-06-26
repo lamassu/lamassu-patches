f794e6c37610fb172800f66e855b8c90f0b1c1a7
CrypticaScriptura <7396812+CrypticaScriptura@users.noreply.github.com>
2023-06-26 14:37:46 -0400

    Update accounts.js

--- switches/admin/mock/accounts.js
+++ switches/admin/mock/accounts.js
@@ -1,7 +1,10 @@
+const { COINS, ALL_CRYPTOS } = require('@lamassu/coins')
 const _ = require('lodash/fp')
-const { COINS, ALL_CRYPTOS } = require('./coins')
 
-const { BTC, BCH, DASH, ETH, LTC, ZEC } = COINS
+const { ALL } = require('../../plugins/common/ccxt')
+
+const { BTC, BCH, DASH, ETH, LTC, USDT, ZEC, XMR } = COINS
+const { bitpay, coinbase, itbit, bitstamp, kraken, binanceus, cex, binance } = ALL
 
 const TICKER = 'ticker'
 const WALLET = 'wallet'
@@ -11,26 +14,34 @@ const SMS = 'sms'
 const ID_VERIFIER = 'idVerifier'
 const EMAIL = 'email'
 const ZERO_CONF = 'zeroConf'
+const WALLET_SCORING = 'wallet_scoring'
 
 const ALL_ACCOUNTS = [
-  { code: 'bitpay', display: 'Bitpay', class: TICKER, cryptos: [BTC, BCH] },
-  { code: 'kraken', display: 'Kraken', class: TICKER, cryptos: [BTC, ETH, LTC, DASH, ZEC, BCH] },
-  { code: 'bitstamp', display: 'Bitstamp', class: TICKER, cryptos: [BTC, ETH, LTC, BCH] },
-  { code: 'coinbase', display: 'Coinbase', class: TICKER, cryptos: [BTC, ETH, LTC, DASH, ZEC, BCH] },
-  { code: 'itbit', display: 'itBit', class: TICKER, cryptos: [BTC, ETH] },
+  { code: 'binance', display: 'Binance', class: TICKER, cryptos: binance.CRYPTO },
+  { code: 'binanceus', display: 'Binance.us', class: TICKER, cryptos: binanceus.CRYPTO },
+  { code: 'cex', display: 'CEX.IO', class: TICKER, cryptos: cex.CRYPTO },
+  { code: 'bitpay', display: 'Bitpay', class: TICKER, cryptos: bitpay.CRYPTO },
+  { code: 'kraken', display: 'Kraken', class: TICKER, cryptos: kraken.CRYPTO },
+  { code: 'bitstamp', display: 'Bitstamp', class: TICKER, cryptos: bitstamp.CRYPTO },
+  { code: 'coinbase', display: 'Coinbase', class: TICKER, cryptos: coinbase.CRYPTO },
+  { code: 'itbit', display: 'itBit', class: TICKER, cryptos: itbit.CRYPTO },
   { code: 'mock-ticker', display: 'Mock (Caution!)', class: TICKER, cryptos: ALL_CRYPTOS, dev: true },
   { code: 'bitcoind', display: 'bitcoind', class: WALLET, cryptos: [BTC] },
   { code: 'no-layer2', display: 'No Layer 2', class: LAYER_2, cryptos: ALL_CRYPTOS },
-  { code: 'infura', display: 'Infura', class: WALLET, cryptos: [ETH] },
-  { code: 'geth', display: 'geth', class: WALLET, cryptos: [ETH] },
+  { code: 'infura', display: 'Infura', class: WALLET, cryptos: [ETH, USDT] },
+  { code: 'geth', display: 'geth (deprecated)', class: WALLET, cryptos: [ETH, USDT] },
   { code: 'zcashd', display: 'zcashd', class: WALLET, cryptos: [ZEC] },
   { code: 'litecoind', display: 'litecoind', class: WALLET, cryptos: [LTC] },
   { code: 'dashd', display: 'dashd', class: WALLET, cryptos: [DASH] },
+  { code: 'monerod', display: 'monerod', class: WALLET, cryptos: [XMR] },
   { code: 'bitcoincashd', display: 'bitcoincashd', class: WALLET, cryptos: [BCH] },
   { code: 'bitgo', display: 'BitGo', class: WALLET, cryptos: [BTC, ZEC, LTC, BCH, DASH] },
-  { code: 'bitstamp', display: 'Bitstamp', class: EXCHANGE, cryptos: [BTC, ETH, LTC, BCH] },
-  { code: 'itbit', display: 'itBit', class: EXCHANGE, cryptos: [BTC, ETH] },
-  { code: 'kraken', display: 'Kraken', class: EXCHANGE, cryptos: [BTC, ETH, LTC, DASH, ZEC, BCH] },
+  { code: 'bitstamp', display: 'Bitstamp', class: EXCHANGE, cryptos: bitstamp.CRYPTO },
+  { code: 'itbit', display: 'itBit', class: EXCHANGE, cryptos: itbit.CRYPTO },
+  { code: 'kraken', display: 'Kraken', class: EXCHANGE, cryptos: kraken.CRYPTO },
+  { code: 'binance', display: 'Binance', class: EXCHANGE, cryptos: binance.CRYPTO },
+  { code: 'binanceus', display: 'Binance.us', class: EXCHANGE, cryptos: binanceus.CRYPTO },
+  { code: 'cex', display: 'CEX.IO', class: EXCHANGE, cryptos: cex.CRYPTO },
   { code: 'mock-wallet', display: 'Mock (Caution!)', class: WALLET, cryptos: ALL_CRYPTOS },
   { code: 'no-exchange', display: 'No exchange', class: EXCHANGE, cryptos: ALL_CRYPTOS },
   { code: 'mock-exchange', display: 'Mock exchange', class: EXCHANGE, cryptos: ALL_CRYPTOS, dev: true },
@@ -38,10 +49,11 @@ const ALL_ACCOUNTS = [
   { code: 'mock-id-verify', display: 'Mock ID verifier', class: ID_VERIFIER, dev: true },
   { code: 'twilio', display: 'Twilio', class: SMS },
   { code: 'mailgun', display: 'Mailgun', class: EMAIL },
-  { code: 'all-zero-conf', display: 'Always 0-conf', class: ZERO_CONF, cryptos: [BTC, ZEC, LTC, DASH, BCH] },
-  { code: 'no-zero-conf', display: 'Always 1-conf', class: ZERO_CONF, cryptos: [ETH] },
+  { code: 'none', display: 'None', class: ZERO_CONF, cryptos: ALL_CRYPTOS },
   { code: 'blockcypher', display: 'Blockcypher', class: ZERO_CONF, cryptos: [BTC] },
-  { code: 'mock-zero-conf', display: 'Mock 0-conf', class: ZERO_CONF, cryptos: [BTC, ZEC, LTC, DASH, BCH, ETH], dev: true }
+  { code: 'mock-zero-conf', display: 'Mock 0-conf', class: ZERO_CONF, cryptos: ALL_CRYPTOS, dev: true },
+  { code: 'ciphertrace', display: 'CipherTrace', class: WALLET_SCORING, cryptos: [BTC, ETH, LTC, BCH] },
+  { code: 'mock-scoring', display: 'Mock scoring', class: WALLET_SCORING, cryptos: ALL_CRYPTOS, dev: true }
 ]
 
 const devMode = require('minimist')(process.argv.slice(2)).dev
