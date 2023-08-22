a83440a837d75ecf25eefdeda0fd216964081077
CrypticaScriptura <7396812+CrypticaScriptura@users.noreply.github.com>
2023-08-22 14:11:41 -0400

    chore: use geth v1.12.2

--- wallets/update/eth.sh
+++ wallets/update/eth.sh
@@ -11,9 +11,9 @@ echo "Updating Ethereum configuration file..."
 curl -#o /etc/supervisor/conf.d/ethereum.conf https://raw.githubusercontent.com/lamassu/lamassu-patches/master/wallets/conf/ethereum.conf >> ${LOG_FILE} 2>&1
 echo
 
-echo "Downloading Geth v1.11.5..."
-sourceHash=$'141369366f1c661aeca2aa41a9e9e2cfdbdc42e6b9b06dd230464aba38be8f76'
-curl -#o /tmp/ethereum.tar.gz https://gethstore.blob.core.windows.net/builds/geth-linux-amd64-1.11.5-a38f4108.tar.gz >> ${LOG_FILE} 2>&1
+echo "Downloading Geth v1.12.2..."
+sourceHash=$'f7e11f679c3d6c04a586ca5ce91b1501f0e14a34e4505c052faa9df8ae7b96d2'
+curl -#o /tmp/ethereum.tar.gz https://gethstore.blob.core.windows.net/builds/geth-linux-amd64-1.12.2-bed84606.tar.gz >> ${LOG_FILE} 2>&1
 hash=$(sha256sum /tmp/ethereum.tar.gz | awk '{print $1}' | sed 's/ *$//g')
 
 if [ $hash != $sourceHash ] ; then
@@ -26,8 +26,8 @@ tar -xzf /tmp/ethereum.tar.gz -C /tmp/ >> ${LOG_FILE} 2>&1
 echo
 
 echo "Updating..."
-cp /tmp/geth-linux-amd64-1.11.5-a38f4108/geth /usr/local/bin/geth >> ${LOG_FILE} 2>&1
-rm -r /tmp/geth-linux-amd64-1.11.5-a38f4108/ >> ${LOG_FILE} 2>&1
+cp /tmp/geth-linux-amd64-1.12.2-bed84606/geth /usr/local/bin/geth >> ${LOG_FILE} 2>&1
+rm -r /tmp/geth-linux-amd64-1.12.2-bed84606/ >> ${LOG_FILE} 2>&1
 rm /tmp/ethereum.tar.gz >> ${LOG_FILE} 2>&1
 echo
 
