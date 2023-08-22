5430b6b0bf9d0b1804d86c6b4def151f8ec25b34
CrypticaScriptura <7396812+CrypticaScriptura@users.noreply.github.com>
2023-08-22 14:09:05 -0400

    chore: user zcashd v5.6.1

--- wallets/update/zec.sh
+++ wallets/update/zec.sh
@@ -7,9 +7,9 @@ echo
 echo "Updating your Zcash wallet. This may take a minute."
 echo
 
-echo "Downloading Zcash v5.4.2..."
-sourceHash=$'bbc339d88a23207f81ff01b91b7a925ab013a855c6691d0f8b3400ebfb149c08'
-curl -#Lo /tmp/zcash.tar.gz https://z.cash/downloads/zcash-5.4.2-linux64-debian-bullseye.tar.gz >> ${LOG_FILE} 2>&1
+echo "Downloading Zcash v5.6.1..."
+sourceHash=$'0144cc75f00460b136103eeb7c629d5af8101d52b9a77524f44305d44cfbc795'
+curl -#Lo /tmp/zcash.tar.gz https://z.cash/downloads/zcash-5.6.1-linux64-debian-bullseye.tar.gz >> ${LOG_FILE} 2>&1
 hash=$(sha256sum /tmp/zcash.tar.gz | awk '{print $1}' | sed 's/ *$//g')
 
 if [ $hash != $sourceHash ] ; then
@@ -22,8 +22,8 @@ tar -xzf /tmp/zcash.tar.gz -C /tmp/ >> ${LOG_FILE} 2>&1
 echo
 
 echo "Updating wallet..."
-cp /tmp/zcash-5.4.2/bin/* /usr/local/bin/ >> ${LOG_FILE} 2>&1
-rm -r /tmp/zcash-5.4.2 >> ${LOG_FILE} 2>&1
+cp /tmp/zcash-5.6.1/bin/* /usr/local/bin/ >> ${LOG_FILE} 2>&1
+rm -r /tmp/zcash-5.6.1 >> ${LOG_FILE} 2>&1
 rm /tmp/zcash.tar.gz >> ${LOG_FILE} 2>&1
 echo
 
