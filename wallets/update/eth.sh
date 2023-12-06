9ef29689300d1047a8cf78af0a232cc582c77fb7
CrypticaScriptura <7396812+CrypticaScriptura@users.noreply.github.com>
2023-12-06 11:10:43 -0500

    chore: update wallet nodes (#40)

--- wallets/update/eth.sh
+++ wallets/update/eth.sh
@@ -11,9 +11,9 @@ echo "Updating Ethereum configuration file..."
 curl -#o /etc/supervisor/conf.d/ethereum.conf https://raw.githubusercontent.com/lamassu/lamassu-patches/master/wallets/conf/ethereum.conf >> ${LOG_FILE} 2>&1
 echo
 
-echo "Downloading Geth v1.12.2..."
-sourceHash=$'f7e11f679c3d6c04a586ca5ce91b1501f0e14a34e4505c052faa9df8ae7b96d2'
-curl -#o /tmp/ethereum.tar.gz https://gethstore.blob.core.windows.net/builds/geth-linux-amd64-1.12.2-bed84606.tar.gz >> ${LOG_FILE} 2>&1
+echo "Downloading Geth v1.13.5..."
+sourceHash=$'e9a7f49e43f8279830d768d68245904ac051c7b338999239a684b03fa9395321'
+curl -#o /tmp/ethereum.tar.gz https://gethstore.blob.core.windows.net/builds/geth-linux-amd64-1.13.5-916d6a44.tar.gz >> ${LOG_FILE} 2>&1
 hash=$(sha256sum /tmp/ethereum.tar.gz | awk '{print $1}' | sed 's/ *$//g')
 
 if [ $hash != $sourceHash ] ; then
@@ -26,8 +26,8 @@ tar -xzf /tmp/ethereum.tar.gz -C /tmp/ >> ${LOG_FILE} 2>&1
 echo
 
 echo "Updating..."
-cp /tmp/geth-linux-amd64-1.12.2-bed84606/geth /usr/local/bin/geth >> ${LOG_FILE} 2>&1
-rm -r /tmp/geth-linux-amd64-1.12.2-bed84606/ >> ${LOG_FILE} 2>&1
+cp /tmp/geth-linux-amd64-1.13.5-916d6a44/geth /usr/local/bin/geth >> ${LOG_FILE} 2>&1
+rm -r /tmp/geth-linux-amd64-1.13.5-916d6a44/ >> ${LOG_FILE} 2>&1
 rm /tmp/ethereum.tar.gz >> ${LOG_FILE} 2>&1
 echo
 
--- wallets/update/xmr.sh
+++ wallets/update/xmr.sh
@@ -7,9 +7,9 @@ echo
 echo "Updating your Monero wallet. This may take a minute."
 echo
 
-echo "Downloading Monero v0.18.2.2..."
-sourceHash=$'186800de18f67cca8475ce392168aabeb5709a8f8058b0f7919d7c693786d56b'
-curl -#Lo /tmp/monero.tar.bz2 https://downloads.getmonero.org/cli/monero-linux-x64-v0.18.2.2.tar.bz2 >> ${LOG_FILE} 2>&1
+echo "Downloading Monero v0.18.3.1..."
+sourceHash=$'23af572fdfe3459b9ab97e2e9aa7e3c11021c955d6064b801a27d7e8c21ae09d'
+curl -#Lo /tmp/monero.tar.bz2 https://downloads.getmonero.org/cli/monero-linux-x64-v0.18.3.1.tar.bz2 >> ${LOG_FILE} 2>&1
 hash=$(sha256sum /tmp/monero.tar.bz2 | awk '{print $1}' | sed 's/ *$//g')
 
 if [ $hash != $sourceHash ] ; then
@@ -22,9 +22,9 @@ tar -xf /tmp/monero.tar.bz2 -C /tmp/ >> ${LOG_FILE} 2>&1
 echo
 
 echo "Updating wallet..."
-cp /tmp/monero-x86_64-linux-gnu-v0.18.2.2/monerod /usr/local/bin/ >> ${LOG_FILE} 2>&1
-cp /tmp/monero-x86_64-linux-gnu-v0.18.2.2/monero-wallet-rpc /usr/local/bin/ >> ${LOG_FILE} 2>&1
-rm -r /tmp/monero-x86_64-linux-gnu-v0.18.2.2 >> ${LOG_FILE} 2>&1
+cp /tmp/monero-x86_64-linux-gnu-v0.18.3.1/monerod /usr/local/bin/ >> ${LOG_FILE} 2>&1
+cp /tmp/monero-x86_64-linux-gnu-v0.18.3.1/monero-wallet-rpc /usr/local/bin/ >> ${LOG_FILE} 2>&1
+rm -r /tmp/monero-x86_64-linux-gnu-v0.18.3.1 >> ${LOG_FILE} 2>&1
 rm /tmp/monero.tar.bz2 >> ${LOG_FILE} 2>&1
 echo
 
