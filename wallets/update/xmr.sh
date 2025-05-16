5cdc91456761140f33fa63788ae95cbbdd2c1f7f
CrypticaScriptura <7396812+CrypticaScriptura@users.noreply.github.com>
2025-05-16 14:25:35 -0400

    chore: update node wallets (#58)

--- wallets/update/xmr.sh
+++ wallets/update/xmr.sh
@@ -7,9 +7,9 @@ echo
 echo "Updating your Monero wallet. This may take a minute."
 echo
 
-echo "Downloading Monero v0.18.3.4..."
-sourceHash=$'51ba03928d189c1c11b5379cab17dd9ae8d2230056dc05c872d0f8dba4a87f1d'
-curl -#Lo /tmp/monero.tar.bz2 https://downloads.getmonero.org/cli/monero-linux-x64-v0.18.3.4.tar.bz2 >> ${LOG_FILE} 2>&1
+echo "Downloading Monero v0.18.4.0..."
+sourceHash=$'16cb74c899922887827845a41d37c7f3121462792a540843f2fcabcc1603993f'
+curl -#Lo /tmp/monero.tar.bz2 https://downloads.getmonero.org/cli/monero-linux-x64-v0.18.4.0.tar.bz2 >> ${LOG_FILE} 2>&1
 hash=$(sha256sum /tmp/monero.tar.bz2 | awk '{print $1}' | sed 's/ *$//g')
 
 if [ $hash != $sourceHash ] ; then
@@ -22,9 +22,9 @@ tar -xf /tmp/monero.tar.bz2 -C /tmp/ >> ${LOG_FILE} 2>&1
 echo
 
 echo "Updating wallet..."
-cp /tmp/monero-x86_64-linux-gnu-v0.18.3.4/monerod /usr/local/bin/ >> ${LOG_FILE} 2>&1
-cp /tmp/monero-x86_64-linux-gnu-v0.18.3.4/monero-wallet-rpc /usr/local/bin/ >> ${LOG_FILE} 2>&1
-rm -r /tmp/monero-x86_64-linux-gnu-v0.18.3.4 >> ${LOG_FILE} 2>&1
+cp /tmp/monero-x86_64-linux-gnu-v0.18.4.0/monerod /usr/local/bin/ >> ${LOG_FILE} 2>&1
+cp /tmp/monero-x86_64-linux-gnu-v0.18.4.0/monero-wallet-rpc /usr/local/bin/ >> ${LOG_FILE} 2>&1
+rm -r /tmp/monero-x86_64-linux-gnu-v0.18.4.0 >> ${LOG_FILE} 2>&1
 rm /tmp/monero.tar.bz2 >> ${LOG_FILE} 2>&1
 echo
 
